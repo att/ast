85769c6d6bf50c1e63486e77f94789b6777187b9
Kurtis Rader <krader@skepticism.us>
2017-10-30 20:11:35 -0700

    Assume `MAP_ANON` is valid
    One of the feature tests involving `mmap()` is dubious since it involves
    highly variable timing data that can lead to non-deterministic builds.
    Several other feature tests are no longer relevant since, for example, I
    can't think of a single OS we care about which supports `mmap()` but not
    the `MAP_ANON` flag.
    
    Finally, there is no good reason to even use `mmap()` and implement our
    own memory allocator. These days we should be using the OS provided libc
    `malloc()` family of functions. And the value of the performance speedup
    of using it in the SFIO code is dubious for a command shell. However, I
    found that when I disabled it completely it results in more test failures
    so this change is limited to the MAP_ANON related configuration.
    
    Fixes #84

--- src/lib/libast/sfio/sfsetfd.c
+++ src/lib/libast/sfio/sfsetfd.c
@@ -108,12 +108,10 @@ int	newfd;
 					SFMTXRETURN(f, -1);
 				}
 
-#if _mmap_worthy
 				if((f->bits&SF_MMAP) && f->data)
 				{	SFMUNMAP(f,f->data,f->endb-f->data);
 					f->data = NIL(uchar*);
 				}
-#endif
 
 				/* make stream appears uninitialized */
 				f->endb = f->endr = f->endw = f->data;
