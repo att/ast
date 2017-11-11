7e4eb6385eac6ffb5b94ec90572fc9c29672504d
Kurtis Rader <krader@skepticism.us>
2017-11-10 21:31:23 -0800

    Fix something involving nv_putval()
    This change is pulled from here:
    
    https://github.com/oracle/solaris-userland/blob/master/components/ksh93/patches/065-CR7110983.patch
    
    It does not break any existing unit tests and looks valid. It is
    unfortunate that the change comment associated with that URL is not more
    descriptive about what is fixed. I'm merging this because I would like
    to make some radical changes to the style of the code which will make
    merging such changes more difficult. So I am implicitly assuming the
    team maintaining ksh on Solaris knew what they were doing.
    
    Fix for #122

--- src/cmd/ksh93/sh/name.c
+++ src/cmd/ksh93/sh/name.c
@@ -2043,8 +2043,10 @@ void nv_putval(register Namval_t *np, const char *string, int flags)
 			else
 #endif
 			{
-				if(size==0 && nv_isattr(np,NV_HOST)!=NV_HOST &&nv_isattr(np,NV_LJUST|NV_RJUST|NV_ZFILL))
+				if(size==0 && nv_isattr(np,NV_HOST)!=NV_HOST &&nv_isattr(np,NV_LJUST|NV_RJUST|NV_ZFILL)) {
 					nv_setsize(np,size=dot);
+					tofree = up->cp;
+				}
 				else if(size > dot)
 					dot = size;
 				else if(nv_isattr(np,NV_LJUST|NV_RJUST)==NV_LJUST && dot>size)
