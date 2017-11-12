9e7ff62221dac0684ebd2654d5770d9e0a16f3e5
Kurtis Rader <krader@skepticism.us>
2017-11-12 14:34:10 -0800

    Fix something involving shell initialization
    This change is pulled from here:
    
    https://github.com/oracle/solaris-userland/blob/master/components/ksh93/patches/115-CR6934836.patch
    
    Fix for #122

--- src/cmd/ksh93/sh/main.c
+++ src/cmd/ksh93/sh/main.c
@@ -302,6 +302,8 @@ int sh_main(int ac, char *av[], Shinit_f userinit)
 					 * try to undo effect of solaris 2.5+
 					 * change for argv for setuid scripts
 					 */
+					if (shp->st.repl_index > 0)
+						av[shp->st.repl_index] = shp->st.repl_arg;
 					if(((type = sh_type(cp = av[0])) & SH_TYPE_SH) && (!(name = nv_getval(L_ARGNOD)) || !((type = sh_type(cp = name)) & SH_TYPE_SH)))
 					{
 						av[0] = (type & SH_TYPE_LOGIN) ? cp : path_basename(cp);
