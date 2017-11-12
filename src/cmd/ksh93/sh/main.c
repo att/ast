c113d34c9460a6ba848398f1a031b100b4b0ccbd
Kurtis Rader <krader@skepticism.us>
2017-11-12 15:00:11 -0800

    Fix something involving argument handling
    This change is pulled from here:
    
    https://github.com/oracle/solaris-userland/blob/master/components/ksh93/patches/190-17432413.patch
    
    Fix for #122

--- src/cmd/ksh93/sh/main.c
+++ src/cmd/ksh93/sh/main.c
@@ -304,7 +304,7 @@ int sh_main(int ac, char *av[], Shinit_f userinit)
 					 */
 					if (shp->st.repl_index > 0)
 						av[shp->st.repl_index] = shp->st.repl_arg;
-					if(((type = sh_type(cp = av[0])) & SH_TYPE_SH) && (!(name = nv_getval(L_ARGNOD)) || !((type = sh_type(cp = name)) & SH_TYPE_SH)))
+					if(((type = sh_type(cp = av[0])) & SH_TYPE_SH) && (name = nv_getval(L_ARGNOD)) && (!((type = sh_type(cp = name)) & SH_TYPE_SH)))
 					{
 						av[0] = (type & SH_TYPE_LOGIN) ? cp : path_basename(cp);
 						/*  exec to change $0 for ps */
