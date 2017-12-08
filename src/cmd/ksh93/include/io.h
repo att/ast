0d15d26f42ebc0e334d7342d18cc6d52744be9f3
Kurtis Rader <krader@skepticism.us>
2017-12-07 17:17:28 -0800

    Pick an unused fd to capture subshell output
    A subshell that manipulates file descriptors, such as redirecting fd 3
    then closing it, interferes with capturing its output. Ensure we use a
    file descriptor that is not in use.
    
    Fix #198

--- src/cmd/ksh93/include/io.h
+++ src/cmd/ksh93/include/io.h
@@ -82,6 +82,7 @@ extern void 	sh_vexsave(Shell_t*,int,int,Spawnvex_f,void*);
 extern Sfio_t 	*sh_iostream(Shell_t*,int,int);
 extern int	sh_redirect(Shell_t*,struct ionod*,int);
 extern void 	sh_iosave(Shell_t *, int,int,char*);
+extern int	sh_get_unused_fd(Shell_t* shp, int min_fd);
 extern bool 	sh_iovalidfd(Shell_t*, int);
 extern bool 	sh_inuse(Shell_t*, int);
 extern void 	sh_iounsave(Shell_t*);
