dc71c7ff3d1ed9ac9a8f20644c50a6ee4e81f66a
Kurtis Rader <krader@skepticism.us>
2018-01-04 14:19:27 -0800

    Run lib/ardir through clang-format

--- src/lib/libardir/testar.c
+++ src/lib/libardir/testar.c
@@ -1,58 +1,53 @@
 /***********************************************************************
-*                                                                      *
-*               This software is part of the ast package               *
-*          Copyright (c) 2002-2011 AT&T Intellectual Property          *
-*                      and is licensed under the                       *
-*                 Eclipse Public License, Version 1.0                  *
-*                    by AT&T Intellectual Property                     *
-*                                                                      *
-*                A copy of the License is available at                 *
-*          http://www.eclipse.org/org/documents/epl-v10.html           *
-*         (with md5 checksum b35adb5213ca9657e911e9befb180842)         *
-*                                                                      *
-*              Information and Software Systems Research               *
-*                            AT&T Research                             *
-*                           Florham Park NJ                            *
-*                                                                      *
-*               Glenn Fowler <glenn.s.fowler@gmail.com>                *
-*                    David Korn <dgkorn@gmail.com>                     *
-*                                                                      *
-***********************************************************************/
-#include <ast.h>
+ *                                                                      *
+ *               This software is part of the ast package               *
+ *          Copyright (c) 2002-2011 AT&T Intellectual Property          *
+ *                      and is licensed under the                       *
+ *                 Eclipse Public License, Version 1.0                  *
+ *                    by AT&T Intellectual Property                     *
+ *                                                                      *
+ *                A copy of the License is available at                 *
+ *          http://www.eclipse.org/org/documents/epl-v10.html           *
+ *         (with md5 checksum b35adb5213ca9657e911e9befb180842)         *
+ *                                                                      *
+ *              Information and Software Systems Research               *
+ *                            AT&T Research                             *
+ *                           Florham Park NJ                            *
+ *                                                                      *
+ *               Glenn Fowler <glenn.s.fowler@gmail.com>                *
+ *                    David Korn <dgkorn@gmail.com>                     *
+ *                                                                      *
+ ***********************************************************************/
 #include <ardir.h>
+#include <ast.h>
 #include <error.h>
 
-main(int argc, char** argv)
-{
-	Ardir_t*	dir;
-	Ardirent_t*	ent;
-	long		touch;
-	char*		file;
+main(int argc, char **argv) {
+    Ardir_t *dir;
+    Ardirent_t *ent;
+    long touch;
+    char *file;
 
-	touch = 0;
-	while (file = *++argv)
-	{
-		if (!strcmp(file, "-t") && *(argv + 1))
-			touch = strtol(*++argv, NULL, 0);
-		else if (dir = ardiropen(file, NULL, touch ? ARDIR_UPDATE : 0))
-		{
-			sfprintf(sfstdout, "%s: type=%s truncate=%d%s\n", file, dir->meth->name, dir->truncate, (dir->flags & ARDIR_RANLIB) ? " ranlib" : "");
-			while (ent = ardirnext(dir))
-			{
-				if (touch)
-				{
-					ent->mtime = touch;
-					ardirchange(dir, ent);
-					sfprintf(sfstdout, "touch %s\n", ent->name);
-				}
-				else
-					sfprintf(sfstdout, "%s %8u %8u %8llu %8llu %s %s\n", fmtmode(ent->mode, 1), ent->uid, ent->gid, ent->size, ent->offset, fmttime("%k", ent->mtime), ent->name);
-			}
-			if (ardirclose(dir))
-				error(2, "%s: archive read error", file);
-		}
-		else
-			error(ERROR_SYSTEM|2, "%s: not an archive", file);
-	}
-	return 0;
+    touch = 0;
+    while (file = *++argv) {
+        if (!strcmp(file, "-t") && *(argv + 1))
+            touch = strtol(*++argv, NULL, 0);
+        else if (dir = ardiropen(file, NULL, touch ? ARDIR_UPDATE : 0)) {
+            sfprintf(sfstdout, "%s: type=%s truncate=%d%s\n", file, dir->meth->name, dir->truncate,
+                     (dir->flags & ARDIR_RANLIB) ? " ranlib" : "");
+            while (ent = ardirnext(dir)) {
+                if (touch) {
+                    ent->mtime = touch;
+                    ardirchange(dir, ent);
+                    sfprintf(sfstdout, "touch %s\n", ent->name);
+                } else
+                    sfprintf(sfstdout, "%s %8u %8u %8llu %8llu %s %s\n", fmtmode(ent->mode, 1),
+                             ent->uid, ent->gid, ent->size, ent->offset, fmttime("%k", ent->mtime),
+                             ent->name);
+            }
+            if (ardirclose(dir)) error(2, "%s: archive read error", file);
+        } else
+            error(ERROR_SYSTEM | 2, "%s: not an archive", file);
+    }
+    return 0;
 }
