2ff70269904ae7fb4182805e0ca7993a1c6e53af
Siteshwar Vashisht <svashisht@redhat.com>
2018-01-04 21:17:29 +0100

    Use $MESON_SOURCE_ROOT to determine source directory
    Adding to incomplete fix in 2a7ec5b3a638705adc7e8298f0e2cf360f69d5ab

--- bin/setup_cmds.sh
+++ bin/setup_cmds.sh
@@ -2,25 +2,11 @@
 set -e
 
 # This script is used to setup iffe and other commands that are required for compilation
-
-# This script is run from an unspecified directory so we have to determine directory paths
-# http://mesonbuild.com/Reference-manual.html#run_command
-if type realpath >/dev/null 2>&1
-then
-    script_path=`realpath "$0"`
-elif type grealpath >/dev/null 2>&1
-then
-    script_path=`grealpath "$0"`
-else
-    echo Sorry but your system does not have a realpath or grealpath command >&2
-    exit 1
-fi
-bin_dir=`dirname "$script_path"`
-base_dir=`dirname "$bin_dir"`
+bin_dir="$MESON_SOURCE_ROOT/bin"
 
 echo "#!/usr/bin/env bash" > "$bin_dir/iffe"
-cat "$base_dir/src/cmd/INIT/iffe.sh" >> "$bin_dir/iffe"
+cat "$MESON_SOURCE_ROOT/src/cmd/INIT/iffe.sh" >> "$bin_dir/iffe"
 chmod u+x "$bin_dir/iffe"
 
-cp "$base_dir/src/lib/libast/comp/conf.sh" "$bin_dir/conf"
+cp "$MESON_SOURCE_ROOT/src/lib/libast/comp/conf.sh" "$bin_dir/conf"
 chmod u+x "$bin_dir/conf"
