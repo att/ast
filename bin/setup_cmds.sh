ba0ea0cde8f131527a3bb1c089aa6ccfb6c77808
Siteshwar Vashisht <svashisht@redhat.com>
2018-01-04 21:27:19 +0100

    Do not copy iffe to bin directory during build
    iffe has been removed from the build process.

--- bin/setup_cmds.sh
+++ bin/setup_cmds.sh
@@ -4,9 +4,5 @@ set -e
 # This script is used to setup iffe and other commands that are required for compilation
 bin_dir="$MESON_SOURCE_ROOT/bin"
 
-echo "#!/usr/bin/env bash" > "$bin_dir/iffe"
-cat "$MESON_SOURCE_ROOT/src/cmd/INIT/iffe.sh" >> "$bin_dir/iffe"
-chmod u+x "$bin_dir/iffe"
-
 cp "$MESON_SOURCE_ROOT/src/lib/libast/comp/conf.sh" "$bin_dir/conf"
 chmod u+x "$bin_dir/conf"
