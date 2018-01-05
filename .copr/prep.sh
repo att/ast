8d37d840c9bdabe20c53b237dd954320f4f7d3c4
Siteshwar Vashisht <svashisht@redhat.com>
2018-01-05 18:48:56 +0100

    Update template for spec file for copr builds
    Also add configurations files for fedora

--- .copr/prep.sh
+++ .copr/prep.sh
@@ -3,10 +3,12 @@
 COMMIT=$(git rev-parse HEAD)
 COMMIT_SHORT=$(git rev-parse --short HEAD)
 COMMIT_NUM=$(git rev-list HEAD --count)
+COMMIT_DATE=$(date --date="@$(git show -s --format=%ct HEAD)" +%Y%m%d)
 
 sed "s,#COMMIT#,${COMMIT},;
      s,#SHORTCOMMIT#,${COMMIT_SHORT},;
-     s,#COMMITNUM#,${COMMIT_NUM}," \
+     s,#COMMITNUM#,${COMMIT_NUM},;
+     s,#COMMITDATE#,${COMMIT_DATE}," \
          packaging/fedora/ksh.spec.in > packaging/fedora/ksh.spec
 
 git archive --prefix "ast-${COMMIT}/" --format "tar.gz" "${COMMIT}" -o "packaging/fedora/ksh-${COMMIT_SHORT}.tar.gz"
