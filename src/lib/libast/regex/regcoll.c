7922c1b7dabfaf3184b0496f2ddca02a5cf371ad
Kurtis Rader <krader@skepticism.us>
2018-01-06 14:45:25 -0800

    Minimal style reformat of libast regex code
    This change is only the result of clang-format with no manual fixup.
    
    Part of resolving #125

--- src/lib/libast/regex/regcoll.c
+++ src/lib/libast/regex/regcoll.c
@@ -1,24 +1,24 @@
 /***********************************************************************
-*                                                                      *
-*               This software is part of the ast package               *
-*          Copyright (c) 1985-2013 AT&T Intellectual Property          *
-*                      and is licensed under the                       *
-*                 Eclipse Public License, Version 1.0                  *
-*                    by AT&T Intellectual Property                     *
-*                                                                      *
-*                A copy of the License is available at                 *
-*          http://www.eclipse.org/org/documents/epl-v10.html           *
-*         (with md5 checksum b35adb5213ca9657e911e9befb180842)         *
-*                                                                      *
-*              Information and Software Systems Research               *
-*                            AT&T Research                             *
-*                           Florham Park NJ                            *
-*                                                                      *
-*               Glenn Fowler <glenn.s.fowler@gmail.com>                *
-*                    David Korn <dgkorn@gmail.com>                     *
-*                     Phong Vo <phongvo@gmail.com>                     *
-*                                                                      *
-***********************************************************************/
+ *                                                                      *
+ *               This software is part of the ast package               *
+ *          Copyright (c) 1985-2013 AT&T Intellectual Property          *
+ *                      and is licensed under the                       *
+ *                 Eclipse Public License, Version 1.0                  *
+ *                    by AT&T Intellectual Property                     *
+ *                                                                      *
+ *                A copy of the License is available at                 *
+ *          http://www.eclipse.org/org/documents/epl-v10.html           *
+ *         (with md5 checksum b35adb5213ca9657e911e9befb180842)         *
+ *                                                                      *
+ *              Information and Software Systems Research               *
+ *                            AT&T Research                             *
+ *                           Florham Park NJ                            *
+ *                                                                      *
+ *               Glenn Fowler <glenn.s.fowler@gmail.com>                *
+ *                    David Korn <dgkorn@gmail.com>                     *
+ *                     Phong Vo <phongvo@gmail.com>                     *
+ *                                                                      *
+ ***********************************************************************/
 #pragma prototyped
 /*
  * regex collation symbol support
@@ -38,85 +38,65 @@
  *		or collating element (*wc == 0) in buf
  */
 
-int
-regcollate(const char* s, char** e, char* buf, size_t size, wchar_t* wc)
-{
-	int			c;
-	char*			b;
-	char*			x;
-	const char*			t;
-	int				i;
-	int				r;
-	int				term;
-	wchar_t				w;
-	Mbstate_t			q;
-	char				xfm[256];
-	char				tmp[sizeof(xfm)];
+int regcollate(const char *s, char **e, char *buf, size_t size, wchar_t *wc) {
+    int c;
+    char *b;
+    char *x;
+    const char *t;
+    int i;
+    int r;
+    int term;
+    wchar_t w;
+    Mbstate_t q;
+    char xfm[256];
+    char tmp[sizeof(xfm)];
 
-	if (size < 2 || (term = *s) != '.' && term != '=' || !*++s || *s == term && *(s + 1) == ']')
-		goto nope;
-	t = s;
-	mbtinit(&q);
-	w = mbtchar(&w, s, MB_LEN_MAX, &q);
-	if ((r = (s - t)) > 1)
-	{
-		if (*s++ != term || *s++ != ']')
-			goto oops;
-		goto done;
-	}
-	if (*s == term && *(s + 1) == ']')
-	{
-		s += 2;
-		goto done;
-	}
-	b = buf;
-	x = buf + size - 2;
-	s = t;
-	for (;;)
-	{
-		if (!(c = *s++))
-			goto oops;
-		if (c == term)
-		{
-			if (!(c = *s++))
-				goto oops;
-			if (c != term)
-			{
-				if (c != ']')
-					goto oops;
-				break;
-			}
-		}
-		if (b < x)
-			*b++ = c;
-	}
-	r = s - t - 2;
-	w = 0;
-	if (b >= x)
-		goto done;
-	*b = 0;
-	for (i = 0; i < r && i < sizeof(tmp) - 1; i++)
-		tmp[i] = '0';
-	tmp[i] = 0;
-	if (mbxfrm(xfm, buf, sizeof(xfm)) >= mbxfrm(xfm, tmp, sizeof(xfm)))
-		goto nope;
-	t = (const char*)buf;
- done:
-	if (r <= size && (char*)t != buf)
-	{
-		memcpy(buf, t, r);
-		if (r < size)
-			buf[r] = 0;
-	}
-	if (wc)
-		*wc = w;
-	if (e)
-		*e = (char*)s;
-	return r;
- oops:
- 	s--;
- nope:
-	if (e)
-		*e = (char*)s;
-	return -1;
+    if (size < 2 || (term = *s) != '.' && term != '=' || !*++s || *s == term && *(s + 1) == ']')
+        goto nope;
+    t = s;
+    mbtinit(&q);
+    w = mbtchar(&w, s, MB_LEN_MAX, &q);
+    if ((r = (s - t)) > 1) {
+        if (*s++ != term || *s++ != ']') goto oops;
+        goto done;
+    }
+    if (*s == term && *(s + 1) == ']') {
+        s += 2;
+        goto done;
+    }
+    b = buf;
+    x = buf + size - 2;
+    s = t;
+    for (;;) {
+        if (!(c = *s++)) goto oops;
+        if (c == term) {
+            if (!(c = *s++)) goto oops;
+            if (c != term) {
+                if (c != ']') goto oops;
+                break;
+            }
+        }
+        if (b < x) *b++ = c;
+    }
+    r = s - t - 2;
+    w = 0;
+    if (b >= x) goto done;
+    *b = 0;
+    for (i = 0; i < r && i < sizeof(tmp) - 1; i++) tmp[i] = '0';
+    tmp[i] = 0;
+    if (mbxfrm(xfm, buf, sizeof(xfm)) >= mbxfrm(xfm, tmp, sizeof(xfm))) goto nope;
+    t = (const char *)buf;
+done:
+    if (r <= size && (char *)t != buf) {
+        memcpy(buf, t, r);
+        if (r < size) buf[r] = 0;
+    }
+    if (wc) *wc = w;
+    if (e) *e = (char *)s;
+    return r;
+oops:
+    s--;
+nope:
+    if (e) *e = (char *)s;
+    return -1;
 }
