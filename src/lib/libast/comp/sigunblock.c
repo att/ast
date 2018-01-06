4bf3a0c75ee9c7f83e8c0cf44d4b51a51b21801e
Kurtis Rader <krader@skepticism.us>
2018-01-06 14:59:12 -0800

    Minimal style reformat of libast comp code
    This change is only the result of clang-format with no manual fixup.
    
    Part of resolving #125

--- src/lib/libast/comp/sigunblock.c
+++ src/lib/libast/comp/sigunblock.c
@@ -1,24 +1,24 @@
 /***********************************************************************
-*                                                                      *
-*               This software is part of the ast package               *
-*          Copyright (c) 1985-2011 AT&T Intellectual Property          *
-*                      and is licensed under the                       *
-*                 Eclipse Public License, Version 1.0                  *
-*                    by AT&T Intellectual Property                     *
-*                                                                      *
-*                A copy of the License is available at                 *
-*          http://www.eclipse.org/org/documents/epl-v10.html           *
-*         (with md5 checksum b35adb5213ca9657e911e9befb180842)         *
-*                                                                      *
-*              Information and Software Systems Research               *
-*                            AT&T Research                             *
-*                           Florham Park NJ                            *
-*                                                                      *
-*               Glenn Fowler <glenn.s.fowler@gmail.com>                *
-*                    David Korn <dgkorn@gmail.com>                     *
-*                     Phong Vo <phongvo@gmail.com>                     *
-*                                                                      *
-***********************************************************************/
+ *                                                                      *
+ *               This software is part of the ast package               *
+ *          Copyright (c) 1985-2011 AT&T Intellectual Property          *
+ *                      and is licensed under the                       *
+ *                 Eclipse Public License, Version 1.0                  *
+ *                    by AT&T Intellectual Property                     *
+ *                                                                      *
+ *                A copy of the License is available at                 *
+ *          http://www.eclipse.org/org/documents/epl-v10.html           *
+ *         (with md5 checksum b35adb5213ca9657e911e9befb180842)         *
+ *                                                                      *
+ *              Information and Software Systems Research               *
+ *                            AT&T Research                             *
+ *                           Florham Park NJ                            *
+ *                                                                      *
+ *               Glenn Fowler <glenn.s.fowler@gmail.com>                *
+ *                    David Korn <dgkorn@gmail.com>                     *
+ *                     Phong Vo <phongvo@gmail.com>                     *
+ *                                                                      *
+ ***********************************************************************/
 #pragma prototyped
 
 #include <ast.h>
@@ -31,20 +31,17 @@ NoN(sigunblock)
 
 #include <sig.h>
 
-int
-sigunblock(int s)
-{
-	int		op;
-	sigset_t	mask;
+int sigunblock(int s) {
+    int op;
+    sigset_t mask;
 
-	sigemptyset(&mask);
-	if (s)
-	{
-		sigaddset(&mask, s);
-		op = SIG_UNBLOCK;
-	}
-	else op = SIG_SETMASK;
-	return(sigprocmask(op, &mask, NULL));
+    sigemptyset(&mask);
+    if (s) {
+        sigaddset(&mask, s);
+        op = SIG_UNBLOCK;
+    } else
+        op = SIG_SETMASK;
+    return (sigprocmask(op, &mask, NULL));
 }
 
 #endif
