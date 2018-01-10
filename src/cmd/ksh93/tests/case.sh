4aa24db8778456a6ec90081a60a86db2f3d8f2fe
Siteshwar Vashisht <svashisht@redhat.com>
2018-01-10 21:57:06 +0100

    Restyle cmd/ksh93/tests/case.sh
    Related: #125

--- src/cmd/ksh93/tests/case.sh
+++ src/cmd/ksh93/tests/case.sh
@@ -17,11 +17,12 @@
 #                    David Korn <dgkorn@gmail.com>                     #
 #                                                                      #
 ########################################################################
+
 function err_exit
 {
-	print -u2 -n "\t"
-	print -u2 -r ${Command}[$1]: "${@:2}"
-	let Errors+=1
+    print -u2 -n "\t"
+    print -u2 -r ${Command}[$1]: "${@:2}"
+    let Errors+=1
 }
 alias err_exit='err_exit $LINENO'
 
@@ -31,52 +32,57 @@ integer Errors=0
 bar=foo2
 bam=foo[3]
 for i in foo1 foo2 foo3 foo4 foo5 foo6
-do	foo=0
-	case $i in
-	foo1)	foo=1;;
-	$bar)	foo=2;;
-	$bam)	foo=3;;
-	foo[4])	foo=4;;
-	${bar%?}5)
-		foo=5;;
-	"${bar%?}6")
-		foo=6;;
-	esac
-	if	[[ $i != foo$foo ]]
-	then	err_exit "$i not matching correct pattern"
-	fi
+do
+    foo=0
+    case $i in
+    foo1)    foo=1;;
+    $bar)    foo=2;;
+    $bam)    foo=3;;
+    foo[4])    foo=4;;
+    ${bar%?}5)
+        foo=5;;
+    "${bar%?}6")
+        foo=6;;
+    esac
+    if [[ $i != foo$foo ]]
+    then
+        err_exit "$i not matching correct pattern"
+    fi
 done
+
 f="[ksh92]"
 case $f in
 \[*\])  ;;
 *)      err_exit "$f does not match \[*\]";;
 esac
 
-if	[[ $($SHELL -c '
-		x=$(case abc {
-			abc)	{ print yes;};;
-			*)	 print no;;
-			}
-		)
-		print -r -- "$x"' 2> /dev/null) != yes ]]
-then err_exit 'case abc {...} not working'
+if [[ $($SHELL -c '
+        x=$(case abc {
+            abc)    { print yes;};;
+            *)     print no;;
+            }
+        )
+        print -r -- "$x"' 2> /dev/null) != yes ]]
+then
+    err_exit 'case abc {...} not working'
 fi
+
 [[ $($SHELL -c 'case a in
 a)      print -n a > /dev/null ;&
 b)      print b;;
 esac') != b ]] && err_exit 'bug in ;& at end of script'
 [[ $(VMDEBUG=1 $SHELL -c '
-	tmp=foo
-	for i in a b
-	do	case $i in
-		a)	:  tmp=$tmp tmp.h=$tmp.h;;
-		b)	( tmp=bar )
-			for j in a
-			do	print -r -- $tmp.h
-			done
-			;;
-		esac
-	done
+    tmp=foo
+    for i in a b
+    do    case $i in
+        a)    :  tmp=$tmp tmp.h=$tmp.h;;
+        b)    ( tmp=bar )
+            for j in a
+            do    print -r -- $tmp.h
+            done
+            ;;
+        esac
+    done
 ') == foo.h ]] || err_exit "optimizer bug"
 
 x=$($SHELL -ec 'case a in a) echo 1; false; echo 2 ;& b) echo 3;; esac')
