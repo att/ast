4ec6edf7b65907a3a8afd3330ec50464eb6049be
Siteshwar Vashisht <svashisht@redhat.com>
2018-01-10 23:29:48 +0100

    Restyle cmd/ksh93/tests/enum.sh
    Related: #125

--- src/cmd/ksh93/tests/enum.sh
+++ src/cmd/ksh93/tests/enum.sh
@@ -19,9 +19,9 @@
 ########################################################################
 function err_exit
 {
-	print -u2 -n "\t"
-	print -u2 -r ${Command}[$1]: "${@:2}"
-	(( Errors+=1 ))
+    print -u2 -n "\t"
+    print -u2 -r ${Command}[$1]: "${@:2}"
+    (( Errors+=1 ))
 }
 alias err_exit='err_exit $LINENO'
 
@@ -31,34 +31,35 @@ enum Color_t=(red green blue orange yellow)
 enum -i Sex_t=(Male Female)
 for ((i=0; i < 1000; i++))
 do
-Color_t x
-[[ $x == red ]] || err_exit 'Color_t does not default to red'
-x=orange
-[[ $x == orange ]] || err_exit '$x should be orange'
-( x=violet) 2> /dev/null && err_exit 'x=violet should fail'
-x[2]=green
-[[ ${x[2]} == green ]] || err_exit '${x[2]} should be green'
-(( x[2] == 1 )) || err_exit '((x[2]!=1))'
-[[ $((x[2])) == 1 ]] || err_exit '$((x[2]))!=1'
-[[ $x == orange ]] || err_exit '$x is no longer orange'
-Color_t -A y
-y[foo]=yellow
-[[ ${y[foo]} == yellow ]] || err_exit '${y[foo]} != yellow'
-(( y[foo] == 4 )) || err_exit '(( y[foo] != 4))'
-unset y
-typeset -a "[Color_t]" z
-z[green]=xyz
-[[ ${z[green]} == xyz ]] || err_exit '${z[green]} should be xyz'
-[[ ${z[1]} == xyz ]] || err_exit '${z[1]} should be xyz'
-z[orange]=bam
-[[ ${!z[@]} == 'green orange' ]] || err_exit '${!z[@]} == "green orange"'
-unset x
-Sex_t x
-[[ $x == Male ]] || err_exit 'Sex_t not defaulting to Male'
-x=female
-[[ $x == Female ]] || err_exit 'Sex_t not case sensitive'
-unset x y z
+    Color_t x
+    [[ $x == red ]] || err_exit 'Color_t does not default to red'
+    x=orange
+    [[ $x == orange ]] || err_exit '$x should be orange'
+    ( x=violet) 2> /dev/null && err_exit 'x=violet should fail'
+    x[2]=green
+    [[ ${x[2]} == green ]] || err_exit '${x[2]} should be green'
+    (( x[2] == 1 )) || err_exit '((x[2]!=1))'
+    [[ $((x[2])) == 1 ]] || err_exit '$((x[2]))!=1'
+    [[ $x == orange ]] || err_exit '$x is no longer orange'
+    Color_t -A y
+    y[foo]=yellow
+    [[ ${y[foo]} == yellow ]] || err_exit '${y[foo]} != yellow'
+    (( y[foo] == 4 )) || err_exit '(( y[foo] != 4))'
+    unset y
+    typeset -a "[Color_t]" z
+    z[green]=xyz
+    [[ ${z[green]} == xyz ]] || err_exit '${z[green]} should be xyz'
+    [[ ${z[1]} == xyz ]] || err_exit '${z[1]} should be xyz'
+    z[orange]=bam
+    [[ ${!z[@]} == 'green orange' ]] || err_exit '${!z[@]} == "green orange"'
+    unset x
+    Sex_t x
+    [[ $x == Male ]] || err_exit 'Sex_t not defaulting to Male'
+    x=female
+    [[ $x == Female ]] || err_exit 'Sex_t not case sensitive'
+    unset x y z
 done
+
 (
 typeset -T X_t=( typeset name=aha )
 typeset -a "[X_t]" arr
@@ -101,9 +102,9 @@ exp='typeset -C c=(bb -a ar=(typeset -a [4]=([5]=ba) );)'
 [[ $(typeset -p c) == "$exp" ]] ||  err_exit 'compound variable contained command array of enums not working'
 
 (
-	set -o nounset
-	bool bl
-	(( bl=true )) 2> /dev/null
+    set -o nounset
+    bool bl
+    (( bl=true )) 2> /dev/null
 ) || err_exit 'set -o nounset causes ((bool=true)) to fail'
 
 bool  -a bl
