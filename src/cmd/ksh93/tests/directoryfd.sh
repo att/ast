bb8824d2034ff9f110657363bf8b5b2b0ad33a18
Siteshwar Vashisht <svashisht@redhat.com>
2018-01-10 23:26:18 +0100

    Restyle cmd/ksh93/tests/directoryfd.sh
    Related: #125

--- src/cmd/ksh93/tests/directoryfd.sh
+++ src/cmd/ksh93/tests/directoryfd.sh
@@ -50,9 +50,9 @@
 # test setup
 function err_exit
 {
-	print -u2 -n '\t'
-	print -u2 -r ${Command}[$1]: "${@:2}"
-	(( Errors++ ))
+    print -u2 -n '\t'
+    print -u2 -r ${Command}[$1]: "${@:2}"
+    (( Errors++ ))
 }
 alias err_exit='err_exit $LINENO'
 
@@ -71,92 +71,90 @@ cd "${tmpdir}" || { err_exit "cd ${tmpdir} failed." ; exit $((Errors<125?Errors:
 
 function test_dirfd_basics1
 {
-	mkdir 'test_dirfd_basics1'
-	cd 'test_dirfd_basics1'
+    mkdir 'test_dirfd_basics1'
+    cd 'test_dirfd_basics1'
 
-	compound out=( typeset stdout stderr ; integer res )
-	compound -r -a tests=(
-		(
-			testname='createfile1_redirect'
-			cmd='redirect {dirfd}<"./tmp" ; print "foo1" >[dirfd]/test1 ; cat tmp/test1'
-			stdoutpattern='foo1'
-		)
-		(
-			testname='createfile1_cmdgroup'
-			cmd='{ print "iam2" >[dirfd]/test1 ; cat tmp/test1 ; } {dirfd}<"./tmp"'
-			stdoutpattern='iam2'
-		)
-		(
-			testname='createfile2_inheritfd_cmdgroup'
-			cmd='{ print "vem1" >[dirfd]/test1 ; dirfd=$dirfd $SHELL -c "cat </proc/\$\$/fd/\$dirfd/test1" ; } {dirfd}<"./tmp"'
-			stdoutpattern='vem1'
-		)
-		(
-			testname='appendfile1_inheritfd_cmdgroup_proc'
-			cmd='{ print -n "foo1_" >[dirfd]/test1 ; dfd=$dirfd $SHELL -c "print appendix1 >>/proc/\$\$/fd/\$dfd/test1" ; } {dirfd}<"./tmp"; cat tmp/test1'
-			stdoutpattern='foo1_appendix1'
-		)
-		(
-			testname='appendfile1_inheritfd_cmdgroup_devfd'
-			cmd='{ print -n "foo2_" >[dirfd]/test1 ; dfd=$dirfd $SHELL -c "print appendix2 >>/dev/fd/\$dfd/test1" ; } {dirfd}<"./tmp"; cat tmp/test1'
-			stdoutpattern='foo2_appendix2'
-		)
-#		TODO: This is a test for mkdir/cat/rmdir builtins. We should reenable it if we choose to make these commands builtin.
-#		(
-#			testname='createdir1_redirect'
-#			cmd='redirect {dirfd}<"./tmp" ; mkdir [dirfd]/test1 ; print "foo2" >tmp/test1/a ; cat [dirfd]/test1/a ; rm tmp/test1/a ; rmdir [dirfd]/test1'
-#			stdoutpattern='foo2'
-#		)
-		(
-			testname='createdir1_cmdgroup'
-			cmd='{ mkdir [dirfd]/test1 ; print "foo3" >tmp/test1/a ; cat [dirfd]/test1/a ; rm tmp/test1/a ; rmdir [dirfd]/test1 ; } {dirfd}<"tmp" ;'
-			stdoutpattern='foo3'
-		)
-	)
-	typeset testname
-	integer i
-	typeset cmd
-	typeset devfd
+    compound out=( typeset stdout stderr ; integer res )
+    compound -r -a tests=(
+        (
+            testname='createfile1_redirect'
+            cmd='redirect {dirfd}<"./tmp" ; print "foo1" >[dirfd]/test1 ; cat tmp/test1'
+            stdoutpattern='foo1'
+        )
+        (
+            testname='createfile1_cmdgroup'
+            cmd='{ print "iam2" >[dirfd]/test1 ; cat tmp/test1 ; } {dirfd}<"./tmp"'
+            stdoutpattern='iam2'
+        )
+        (
+            testname='createfile2_inheritfd_cmdgroup'
+            cmd='{ print "vem1" >[dirfd]/test1 ; dirfd=$dirfd $SHELL -c "cat </proc/\$\$/fd/\$dirfd/test1" ; } {dirfd}<"./tmp"'
+            stdoutpattern='vem1'
+        )
+        (
+            testname='appendfile1_inheritfd_cmdgroup_proc'
+            cmd='{ print -n "foo1_" >[dirfd]/test1 ; dfd=$dirfd $SHELL -c "print appendix1 >>/proc/\$\$/fd/\$dfd/test1" ; } {dirfd}<"./tmp"; cat tmp/test1'
+            stdoutpattern='foo1_appendix1'
+        )
+        (
+            testname='appendfile1_inheritfd_cmdgroup_devfd'
+            cmd='{ print -n "foo2_" >[dirfd]/test1 ; dfd=$dirfd $SHELL -c "print appendix2 >>/dev/fd/\$dfd/test1" ; } {dirfd}<"./tmp"; cat tmp/test1'
+            stdoutpattern='foo2_appendix2'
+        )
+#        TODO: This is a test for mkdir/cat/rmdir builtins. We should reenable it if we choose to make these commands builtin.
+#        (
+#            testname='createdir1_redirect'
+#            cmd='redirect {dirfd}<"./tmp" ; mkdir [dirfd]/test1 ; print "foo2" >tmp/test1/a ; cat [dirfd]/test1/a ; rm tmp/test1/a ; rmdir [dirfd]/test1'
+#            stdoutpattern='foo2'
+#        )
+        (
+            testname='createdir1_cmdgroup'
+            cmd='{ mkdir [dirfd]/test1 ; print "foo3" >tmp/test1/a ; cat [dirfd]/test1/a ; rm tmp/test1/a ; rmdir [dirfd]/test1 ; } {dirfd}<"tmp" ;'
+            stdoutpattern='foo3'
+        )
+    )
+    typeset testname
+    integer i
+    typeset cmd
+    typeset devfd
 
-        for (( i=0 ; i < ${#tests[@]} ; i++ )) ; do
-		nameref tst=tests[i]
+    for (( i=0 ; i < ${#tests[@]} ; i++ )) ; do
+        nameref tst=tests[i]
+    
+        # iterate over all names libast uses to emulate /dev/fd/$fd/<path>
+        for devfd in \
+            '/proc/$$/fd/${dirfd}/' \
+            '/proc/self/fd/${dirfd}/' \
+            '/dev/fd/${dirfd}/' \
+            '~{dirfd}/' ; do
+            cmd="${tst.cmd//\[dirfd\]/${devfd}} ; true"
+            testname="${0}/${tst.testname}/${cmd}"
 
-		# iterate over all names libast uses to emulate /dev/fd/$fd/<path>
-		for devfd in \
-			'/proc/$$/fd/${dirfd}/' \
-			'/proc/self/fd/${dirfd}/' \
-			'/dev/fd/${dirfd}/' \
-			'~{dirfd}/' ; do
-			cmd="${tst.cmd//\[dirfd\]/${devfd}} ; true"
-			testname="${0}/${tst.testname}/${cmd}"
+            mkdir 'tmp'
 
-			mkdir 'tmp'
+            out.stderr="${ { out.stdout="${ ${SHELL} -o nounset -o errexit -c "${cmd}" ; (( out.res=$? )) ; }" ; } 2>&1 ; }"
 
-			out.stderr="${ { out.stdout="${ ${SHELL} -o nounset -o errexit -c "${cmd}" ; (( out.res=$? )) ; }" ; } 2>&1 ; }"
+            rm -Rf 'tmp'
 
-			rm -Rf 'tmp'
+            [[ "${out.stdout}" == ${tst.stdoutpattern}      ]] || err_exit "${testname}: Expected stdout to match $(printf '%q\n' "${tst.stdoutpattern}"), got $(printf '%q\n' "${out.stdout}")"
+            [[ "${out.stderr}" == ''                        ]] || err_exit "${testname}: Expected empty stderr, got $(printf '%q\n' "${out.stderr}")"
+            (( out.res == 0 )) || err_exit "${testname}: Unexpected exit code ${out.res}"
+        done
+    done
 
-			[[ "${out.stdout}" == ${tst.stdoutpattern}      ]] || err_exit "${testname}: Expected stdout to match $(printf '%q\n' "${tst.stdoutpattern}"), got $(printf '%q\n' "${out.stdout}")"
-			[[ "${out.stderr}" == ''                        ]] || err_exit "${testname}: Expected empty stderr, got $(printf '%q\n' "${out.stderr}")"
-			(( out.res == 0 )) || err_exit "${testname}: Unexpected exit code ${out.res}"
-		done
-	done
+    cd '..'
+    rmdir 'test_dirfd_basics1' || err_exit "${0}: Cannot remove directory."
 
-	cd '..'
-	rmdir 'test_dirfd_basics1' || err_exit "${0}: Cannot remove directory."
-
-	return 0
+    return 0
 }
 
 
 # run tests
 test_dirfd_basics1
 
-
 # cleanup
 cd "${ocwd}"
 rmdir "${tmpdir}" || err_exit "Cannot remove temporary directory ${tmpdir}."
-
 
 # tests done
 exit $((Errors<125?Errors:125))
