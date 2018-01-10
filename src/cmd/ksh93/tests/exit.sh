86794d365321edd5897aced1e343517ccb449ec6
Siteshwar Vashisht <svashisht@redhat.com>
2018-01-10 23:35:51 +0100

    Restyle cmd/ksh93/tests/exit.sh
    Related: #125

--- src/cmd/ksh93/tests/exit.sh
+++ src/cmd/ksh93/tests/exit.sh
@@ -19,9 +19,9 @@
 ########################################################################
 function err_exit
 {
-	print -u2 -n "\t"
-	print -u2 -r ${Command}[$1]: "${@:2}"
-	let Errors+=1
+    print -u2 -n "\t"
+    print -u2 -r ${Command}[$1]: "${@:2}"
+    let Errors+=1
 }
 alias err_exit='err_exit $LINENO'
 
@@ -33,14 +33,16 @@ trap "cd /; rm -rf $tmp" EXIT
 
 function abspath
 {
-        base=$(basename $SHELL)
-        cd ${SHELL%/$base}
-        newdir=$(pwd)
-        cd ~-
-        print $newdir/$base
+    base=$(basename $SHELL)
+    cd ${SHELL%/$base}
+    newdir=$(pwd)
+    cd ~-
+    print $newdir/$base
 }
-#test for proper exit of shell
-builtin getconf
+
+# Test for proper exit of shell
+
+# builtin getconf
 ABSHELL=$(abspath)
 cd $tmp || { err_exit "cd $tmp failed"; exit 1; }
 print exit 0 >.profile
@@ -49,39 +51,47 @@ HOME=$PWD \
 PATH=$PATH \
 SHELL=$ABSSHELL \
 $(
-	v=$(getconf LIBPATH)
-	for v in ${v//,/ }
-	do	v=${v#*:}
-		v=${v%%:*}
-		eval [[ \$$v ]] && eval print -n \" \"\$v=\"\$$v\"
-	done
+    v=$(getconf LIBPATH)
+    for v in ${v//,/ }
+    do    v=${v#*:}
+        v=${v%%:*}
+        eval [[ \$$v ]] && eval print -n \" \"\$v=\"\$$v\"
+    done
 ) \
 exec -c -a -ksh ${ABSHELL} -c "exit 1" 1>/dev/null 2>&1
 !
 status=$(echo $?)
-if	[[ -o noprivileged && $status != 0 ]]
-then	err_exit 'exit in .profile is ignored'
-elif	[[ -o privileged && $status == 0 ]]
-then	err_exit 'privileged .profile not ignored'
+if [[ -o noprivileged && $status != 0 ]]
+then
+    err_exit 'exit in .profile is ignored'
+elif [[ -o privileged && $status == 0 ]]
+then
+    err_exit 'privileged .profile not ignored'
 fi
-if	[[ $(trap 'code=$?; echo $code; trap 0; exit $code' 0; exit 123) != 123 ]]
-then	err_exit 'exit not setting $?'
+
+if [[ $(trap 'code=$?; echo $code; trap 0; exit $code' 0; exit 123) != 123 ]]
+then
+    err_exit 'exit not setting $?'
 fi
+
 cat > run.sh <<- "EOF"
-	trap 'code=$?; echo $code; trap 0; exit $code' 0
-	( trap 0; exit 123 )
+    trap 'code=$?; echo $code; trap 0; exit $code' 0
+    ( trap 0; exit 123 )
 EOF
-if	[[ $($SHELL ./run.sh) != 123 ]]
-then	err_exit 'subshell trap on exit overwrites parent trap'
+if [[ $($SHELL ./run.sh) != 123 ]]
+then
+    err_exit 'subshell trap on exit overwrites parent trap'
 fi
+
 cd ~- || err_exit "cd back failed"
 $SHELL -c 'builtin -f cmd getconf; getconf --"?-version"; exit 0' >/dev/null 2>&1 || err_exit 'ksh plugin exit failed -- was ksh built with CCFLAGS+=$(CC.EXPORT.DYNAMIC)?'
 
-#foo=`ls *foo 2> /dev/null | wc -l`
-#status=$?
-#if [[ $status -ne 0 ]]
-#then
-#	err_exit "Wrong return code from a pipe in command"
-#fi
+# TODO: RHBZ#1117316 - Fix and enable this test case 
+# foo=`ls *foo 2> /dev/null | wc -l`
+# status=$?
+# if [[ $status -ne 0 ]]
+# then
+#     err_exit "Wrong return code from a pipe in command"
+# fi
 
 exit $((Errors<125?Errors:125))
