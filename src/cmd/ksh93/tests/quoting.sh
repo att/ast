aeb5e50dfe52f2a534f3f3ed1a4b703e37b795e9
Siteshwar Vashisht <svashisht@redhat.com>
2018-01-11 15:50:55 +0100

    Restyle cmd/ksh93/tests/quoting.sh
    Related: #125

--- src/cmd/ksh93/tests/quoting.sh
+++ src/cmd/ksh93/tests/quoting.sh
@@ -17,148 +17,217 @@
 #                    David Korn <dgkorn@gmail.com>                     #
 #                                                                      #
 ########################################################################
+
 function err_exit
 {
-	print -u2 -n "\t"
-	print -u2 -r ${Command}[$1]: "${@:2}"
-	(( Errors++ ))
+    print -u2 -n "\t"
+    print -u2 -r ${Command}[$1]: "${@:2}"
+    (( Errors++ ))
 }
 alias err_exit='err_exit $LINENO'
 
 Command=${0##*/}
 integer Errors=0
-if	[[ 'hi there' != "hi there" ]]
-then	err_exit "single quotes not the same as double quotes"
+if [[ 'hi there' != "hi there" ]]
+then
+    err_exit "single quotes not the same as double quotes"
 fi
+
 x='hi there'
-if	[[ $x != 'hi there' ]]
-then	err_exit "$x not the same as 'hi there'"
+if [[ $x != 'hi there' ]]
+then
+    err_exit "$x not the same as 'hi there'"
 fi
-if	[[ $x != "hi there" ]]
-then	err_exit "$x not the same as \"hi there \""
+
+if [[ $x != "hi there" ]]
+then
+    err_exit "$x not the same as \"hi there \""
 fi
-if	[[ \a\b\c\*\|\"\ \\ != 'abc*|" \' ]]
-then	err_exit " \\ differs from '' "
+
+if [[ \a\b\c\*\|\"\ \\ != 'abc*|" \' ]]
+then
+    err_exit " \\ differs from '' "
 fi
-if	[[ "ab\'\"\$(" != 'ab\'\''"$(' ]]
-then	err_exit " \"\" differs from '' "
+
+if [[ "ab\'\"\$(" != 'ab\'\''"$(' ]]
+then
+    err_exit " \"\" differs from '' "
 fi
-if	[[ $(print -r - 'abc*|" \') !=  'abc*|" \' ]]
-then	err_exit "\$(print -r - '') differs from ''"
+
+if [[ $(print -r - 'abc*|" \') !=  'abc*|" \' ]]
+then
+    err_exit "\$(print -r - '') differs from ''"
 fi
-if	[[ $(print -r - "abc*|\" \\") !=  'abc*|" \' ]]
-then	err_exit "\$(print -r - '') differs from ''"
+
+if [[ $(print -r - "abc*|\" \\") !=  'abc*|" \' ]]
+then
+    err_exit "\$(print -r - '') differs from ''"
 fi
-if	[[ "$(print -r - 'abc*|" \')" !=  'abc*|" \' ]]
-then	err_exit "\"\$(print -r - '')\" differs from ''"
+
+if [[ "$(print -r - 'abc*|" \')" !=  'abc*|" \' ]]
+then
+    err_exit "\"\$(print -r - '')\" differs from ''"
 fi
-if	[[ "$(print -r - "abc*|\" \\")" !=  'abc*|" \' ]]
-then	err_exit "\"\$(print -r - "")\" differs from ''"
+
+if [[ "$(print -r - "abc*|\" \\")" !=  'abc*|" \' ]]
+then
+    err_exit "\"\$(print -r - "")\" differs from ''"
 fi
-if	[[ $(print -r - $(print -r - 'abc*|" \')) !=  'abc*|" \' ]]
-then	err_exit "nested \$(print -r - '') differs from ''"
+
+if [[ $(print -r - $(print -r - 'abc*|" \')) !=  'abc*|" \' ]]
+then
+    err_exit "nested \$(print -r - '') differs from ''"
 fi
-if	[[ "$(print -r - $(print -r - 'abc*|" \'))" !=  'abc*|" \' ]]
-then	err_exit "\"nested \$(print -r - '')\" differs from ''"
+
+if [[ "$(print -r - $(print -r - 'abc*|" \'))" !=  'abc*|" \' ]]
+then
+    err_exit "\"nested \$(print -r - '')\" differs from ''"
 fi
-if	[[ $(print -r - "$(print -r - 'abc*|" \')") !=  'abc*|" \' ]]
-then	err_exit "nested \"\$(print -r - '')\" differs from ''"
+
+if [[ $(print -r - "$(print -r - 'abc*|" \')") !=  'abc*|" \' ]]
+then
+    err_exit "nested \"\$(print -r - '')\" differs from ''"
 fi
+
 unset x
-if	[[ ${x-$(print -r - "abc*|\" \\")} !=  'abc*|" \' ]]
-then	err_exit "\${x-\$(print -r - '')} differs from ''"
+if [[ ${x-$(print -r - "abc*|\" \\")} !=  'abc*|" \' ]]
+then
+    err_exit "\${x-\$(print -r - '')} differs from ''"
 fi
-if	[[ ${x-$(print -r - "a}c*|\" \\")} !=  'a}c*|" \' ]]
-then	err_exit "\${x-\$(print -r - '}')} differs from ''"
+
+if [[ ${x-$(print -r - "a}c*|\" \\")} !=  'a}c*|" \' ]]
+then
+    err_exit "\${x-\$(print -r - '}')} differs from ''"
 fi
+
 x=$((echo foo)|(cat))
-if	[[ $x != foo  ]]
-then	err_exit "((cmd)|(cmd)) failed"
+if [[ $x != foo  ]]
+then
+    err_exit "((cmd)|(cmd)) failed"
 fi
+
 x=$(print -r -- "\"$HOME\"")
-if	[[ $x != '"'$HOME'"' ]]
-then	err_exit "nested double quotes failed"
+if [[ $x != '"'$HOME'"' ]]
+then
+    err_exit "nested double quotes failed"
 fi
+
 unset z
 : ${z="a{b}c"}
-if	[[ $z != 'a{b}c' ]]
-then	err_exit '${z="a{b}c"} not correct'
+if [[ $z != 'a{b}c' ]]
+then
+    err_exit '${z="a{b}c"} not correct'
 fi
+
 unset z
 : "${z="a{b}c"}"
-if	[[ $z != 'a{b}c' ]]
-then	err_exit '"${z="a{b}c"}" not correct'
+if [[ $z != 'a{b}c' ]]
+then
+    err_exit '"${z="a{b}c"}" not correct'
 fi
-if	[[ $(print -r -- "a\*b") !=  'a\*b' ]]
-then	err_exit '$(print -r -- "a\*b") differs from  a\*b'
+
+if [[ $(print -r -- "a\*b") !=  'a\*b' ]]
+then
+    err_exit '$(print -r -- "a\*b") differs from  a\*b'
 fi
+
 unset x
-if	[[ $(print -r -- "a\*b$x") !=  'a\*b' ]]
-then	err_exit '$(print -r -- "a\*b$x") differs from  a\*b'
+if [[ $(print -r -- "a\*b$x") !=  'a\*b' ]]
+then
+    err_exit '$(print -r -- "a\*b$x") differs from  a\*b'
 fi
+
 x=hello
 set -- ${x+foo bar bam}
-if	(( $# !=3 ))
-then	err_exit '${x+foo bar bam} does not yield three arguments'
+if (( $# !=3 ))
+then
+    err_exit '${x+foo bar bam} does not yield three arguments'
 fi
+
 set -- ${x+foo "bar bam"}
-if	(( $# !=2 ))
-then	err_exit '${x+foo "bar bam"} does not yield two arguments'
+if (( $# !=2 ))
+then
+    err_exit '${x+foo "bar bam"} does not yield two arguments'
 fi
+
 set -- ${x+foo 'bar bam'}
-if	(( $# !=2 ))
-then	err_exit '${x+foo '\''bar bam'\''} does not yield two arguments'
+if (( $# !=2 ))
+then
+    err_exit '${x+foo '\''bar bam'\''} does not yield two arguments'
 fi
+
 set -- ${x+foo $x bam}
-if	(( $# !=3 ))
-then	err_exit '${x+foo $x bam} does not yield three arguments'
+if (( $# !=3 ))
+then
+    err_exit '${x+foo $x bam} does not yield three arguments'
 fi
+
 set -- ${x+foo "$x" bam}
-if	(( $# !=3 ))
-then	err_exit '${x+foo "$x" bam} does not yield three arguments'
+if (( $# !=3 ))
+then
+    err_exit '${x+foo "$x" bam} does not yield three arguments'
 fi
+
 set -- ${x+"foo $x bam"}
-if	(( $# !=1 ))
-then	err_exit '${x+"foo $x bam"} does not yield one argument'
+if (( $# !=1 ))
+then
+    err_exit '${x+"foo $x bam"} does not yield one argument'
 fi
+
 set -- "${x+foo $x bam}"
-if	(( $# !=1 ))
-then	err_exit '"${x+foo $x bam}" does not yield one argument'
+if (( $# !=1 ))
+then
+    err_exit '"${x+foo $x bam}" does not yield one argument'
 fi
+
 set -- ${x+foo "$x "bam}
-if	(( $# !=2 ))
-then	err_exit '${x+foo "$x "bam} does not yield two arguments'
+if (( $# !=2 ))
+then
+    err_exit '${x+foo "$x "bam} does not yield two arguments'
 fi
+
 x="ab$'cd"
-if	[[ $x != 'ab$'"'cd" ]]
-then	err_exit '$'"' inside double quotes not working"
+if [[ $x != 'ab$'"'cd" ]]
+then
+    err_exit '$'"' inside double quotes not working"
 fi
+
 x=`print 'ab$'`
-if	[[ $x != 'ab$' ]]
-then	err_exit '$'"' inside `` quotes not working"
+if [[ $x != 'ab$' ]]
+then
+    err_exit '$'"' inside `` quotes not working"
 fi
+
 unset a
 x=$(print -r -- "'\
 \
 ")
-if	[[ $x != "'" ]]
-then	err_exit 'line continuation in double strings not working'
+if [[ $x != "'" ]]
+then
+    err_exit 'line continuation in double strings not working'
 fi
+
 x=$(print -r -- "'\
 $a\
 ")
-if	[[ $x != "'" ]]
-then	err_exit 'line continuation in expanded double strings not working'
+if [[ $x != "'" ]]
+then
+    err_exit 'line continuation in expanded double strings not working'
 fi
+
 x='\*'
-if	[[ $(print -r -- $x) != '\*' ]]
-then	err_exit 'x="\\*";$x != \*'
+if [[ $(print -r -- $x) != '\*' ]]
+then
+    err_exit 'x="\\*";$x != \*'
 fi
+
 x='   hello    world    '
 set -- $x
-if	(( $# != 2 ))
-then	err_exit 'field splitting error'
+if (( $# != 2 ))
+then
+    err_exit 'field splitting error'
 fi
+
 x=$(print -r -- '1234567890123456789012345678901234567890123456789012345678901234567890 \
 1234567890123456789012345678901234567890123456789012345678901234567890 \
 1234567890123456789012345678901234567890123456789012345678901234567890 \
@@ -174,94 +243,138 @@ x=$(print -r -- '123456789012345678901234567890123456789012345678901234567890123
 1234567890123456789012345678901234567890123456789012345678901234567890 \
 1234567890123456789012345678901234567890123456789012345678901234567890 \
 1234567890123456789012345678901234567890123456789012345678901234567890')
-if	(( ${#x} != (15*73-3) ))
-then	err_exit "length of x, ${#x}, is incorrect should be $((15*73-3))"
+if (( ${#x} != (15*73-3) ))
+then
+    err_exit "length of x, ${#x}, is incorrect should be $((15*73-3))"
 fi
+
 x='$hi'
-if	[[ $x\$ != '$hi$' ]]
-then	err_exit ' $x\$, with x=$hi, does not expand to $hi$'
+if [[ $x\$ != '$hi$' ]]
+then
+    err_exit ' $x\$, with x=$hi, does not expand to $hi$'
 fi
-if	[[ $x$ != '$hi$' ]]
-then	err_exit ' $x$, with x=$hi, does not expand to $hi$'
+
+if [[ $x$ != '$hi$' ]]
+then
+    err_exit ' $x$, with x=$hi, does not expand to $hi$'
 fi
+
 set -- $(/bin/echo foo;sleep 1;/bin/echo bar)
-if	[[ $# != 2 ]]
-then	err_exit 'word splitting after command substitution not working'
+if [[ $# != 2 ]]
+then
+    err_exit 'word splitting after command substitution not working'
 fi
+
 unset q
-if	[[ "${q:+'}q${q:+'}" != q ]]
-then	err_exit 'expansion of "{q:+'\''}" not correct when q unset'
+if [[ "${q:+'}q${q:+'}" != q ]]
+then
+    err_exit 'expansion of "{q:+'\''}" not correct when q unset'
 fi
+
 q=1
-if	[[ "${q:+'}q${q:+'}" != "'q'" ]]
-then	err_exit 'expansion of "{q:+'\''}" not correct when q set'
+if [[ "${q:+'}q${q:+'}" != "'q'" ]]
+then
+    err_exit 'expansion of "{q:+'\''}" not correct when q set'
 fi
+
 x=$'x\' #y'
-if	[[ $x != "x' #y" ]]
-then	err_exit "$'x\' #y'" not working
+if [[ $x != "x' #y" ]]
+then
+    err_exit "$'x\' #y'" not working
 fi
+
 x=$q$'x\' #y'
-if	[[ $x != "1x' #y" ]]
-then	err_exit "$q$'x\' #y'" not working
+if [[ $x != "1x' #y" ]]
+then
+    err_exit "$q$'x\' #y'" not working
 fi
+
 IFS=,
 x='a,b\,c,d'
 set -- $x
-if	[[ $2 != 'b\' ]]
-then	err_exit "field splitting of $x with IFS=$IFS not working"
+if [[ $2 != 'b\' ]]
+then
+    err_exit "field splitting of $x with IFS=$IFS not working"
 fi
+
 foo=bar
 bar=$(print -r -- ${foo+\\n\ })
-if	[[ $bar != '\n ' ]]
-then	err_exit '${foo+\\n\ } expansion error'
+if [[ $bar != '\n ' ]]
+then
+    err_exit '${foo+\\n\ } expansion error'
 fi
+
 unset bar
 bar=$(print -r -- ${foo+\\n\ $bar})
-if	[[ $bar != '\n ' ]]
-then	err_exit '${foo+\\n\ $bar} expansion error with bar unset'
+if [[ $bar != '\n ' ]]
+then
+    err_exit '${foo+\\n\ $bar} expansion error with bar unset'
 fi
+
 x='\\(..\\)|&\|\|\\&\\|'
-if	[[ $(print -r -- $x) != "$x" ]]
-then	err_exit '$x, where x=\\(..\\)|&\|\|\\&\\| not working'
+if [[ $(print -r -- $x) != "$x" ]]
+then
+    err_exit '$x, where x=\\(..\\)|&\|\|\\&\\| not working'
 fi
+
 x='\\('
-if	[[ $(print -r -- a${x}b) != a"${x}"b ]]
-then	err_exit 'a${x}b, where x=\\( not working'
+if [[ $(print -r -- a${x}b) != a"${x}"b ]]
+then
+    err_exit 'a${x}b, where x=\\( not working'
 fi
+
 x=
-if	[[ $(print -r -- $x'\\1') != '\\1' ]]
-then	err_exit 'backreference inside single quotes broken'
+if [[ $(print -r -- $x'\\1') != '\\1' ]]
+then
+    err_exit 'backreference inside single quotes broken'
 fi
+
 set -- ''
 set -- "$@"
-if	(( $# != 1 ))
-then	err_exit '"$@" not preserving nulls'
+if (( $# != 1 ))
+then
+    err_exit '"$@" not preserving nulls'
 fi
+
 x=
-if	[[ $(print -r s"!\2${x}\1\a!") != 's!\2\1\a!' ]]
-then	err_exit  'print -r s"!\2${x}\1\a!" not equal s!\2\1\a!'
+if [[ $(print -r s"!\2${x}\1\a!") != 's!\2\1\a!' ]]
+then
+    err_exit  'print -r s"!\2${x}\1\a!" not equal s!\2\1\a!'
 fi
-if	[[ $(print -r $'foo\n\n\n') != foo ]]
-then	err_exit 'trailing newlines on comsubstitution not removed'
+
+if [[ $(print -r $'foo\n\n\n') != foo ]]
+then
+    err_exit 'trailing newlines on comsubstitution not removed'
 fi
+
 unset x
-if	[[ ${x:='//'} != '//' ]]
-then	err_exit '${x:='//'} != "//"'
+if [[ ${x:='//'} != '//' ]]
+then
+    err_exit '${x:='//'} != "//"'
 fi
-if	[[ $(print -r "\"hi$\"")	!= '"hi$"' ]]
-then	err_exit '$\ not correct inside ""'
+
+if [[ $(print -r "\"hi$\"")    != '"hi$"' ]]
+then
+    err_exit '$\ not correct inside ""'
 fi
+
 unset x
-if	[[ "${x-a\}b}" != 'a}b' ]]
-then	err_exit '"${x-a\}b}" !=  "a}b"'
+if [[ "${x-a\}b}" != 'a}b' ]]
+then
+    err_exit '"${x-a\}b}" !=  "a}b"'
 fi
-if	[[ "\}\]$x\*\{\[\\" != '\}\]\*\{\[\' ]]
-then	err_exit '"\}\]$x\*\{\[\\" !=  "\}\]\*\{\[\"'
+
+if [[ "\}\]$x\*\{\[\\" != '\}\]\*\{\[\' ]]
+then
+    err_exit '"\}\]$x\*\{\[\\" !=  "\}\]\*\{\[\"'
 fi
+
 foo=yes
-if	[[ $(print -r -- {\$foo}) != '{$foo}' ]]
-then	err_exit '{\$foo}' not expanded correctly
+if [[ $(print -r -- {\$foo}) != '{$foo}' ]]
+then
+    err_exit '{\$foo}' not expanded correctly
 fi
+
 [[ foo == $(
 ###########################################################
 ###########################################################
