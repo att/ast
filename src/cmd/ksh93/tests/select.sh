4ce5476ef2c322c1ce0bed07cbe4c519606e4d00
Siteshwar Vashisht <svashisht@redhat.com>
2018-01-11 02:10:33 +0100

    Restyle cmd/ksh93/tests/select.sh
    Related: #125

--- src/cmd/ksh93/tests/select.sh
+++ src/cmd/ksh93/tests/select.sh
@@ -17,11 +17,12 @@
 #                    David Korn <dgkorn@gmail.com>                     #
 #                                                                      #
 ########################################################################
+
 function err_exit
 {
-	print -u2 -n "\t"
-	print -u2 -r ${Command}[$1]: "${@:2}"
-	let Errors+=1
+    print -u2 -n "\t"
+    print -u2 -r ${Command}[$1]: "${@:2}"
+    let Errors+=1
 }
 alias err_exit='err_exit $LINENO'
 
@@ -40,26 +41,33 @@ cat > $tmp/1 <<\!
 !
 
 select i in foo bar bam
-do	case $i in
-	foo)	break;;
-	*)	err_exit "select 1 not working"
-		break;;
-	esac
+do
+    case $i in
+        foo)
+            break;;
+        *)
+            err_exit "select 1 not working"
+            break;;
+    esac
 done 2> /dev/null <<!
 1
 !
 
 unset i
 select i in foo bar bam
-do	case $i in
-	foo)	err_exit "select foo not working" 2>&3
-		break;;
-	*)	if	[[ $REPLY != foo ]]
-		then	err_exit "select REPLY not correct" 2>&3
-		fi
-		( set -u; : $i ) || err_exit "select: i not set to null" 2>&3
-		break;;
-	esac
+do
+    case $i in
+    foo)
+        err_exit "select foo not working" 2>&3
+        break;;
+    *)
+        if [[ $REPLY != foo ]]
+        then
+            err_exit "select REPLY not correct" 2>&3
+        fi
+        ( set -u; : $i ) || err_exit "select: i not set to null" 2>&3
+        break;;
+    esac
 done  3>&2 2> $tmp/2 <<!
 foo
 !
