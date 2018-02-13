eabf79250aef41aa0fd494a0d067a19ed6714181
Kurtis Rader <krader@skepticism.us>
2018-02-12 20:35:04 -0800

    Deal with systems that don't have `clock_gettime()`
    Resolves #404

--- src/lib/libast/tm/tvgettime.c
+++ src/lib/libast/tm/tvgettime.c
@@ -21,8 +21,12 @@
  ***********************************************************************/
 #include "config_ast.h"  // IWYU pragma: keep
 
-#include <tm.h>
-#include <tv.h>
+#include <time.h>
+
+#include "tm.h"
+#include "tv.h"
+
+#if _lib_clock_gettime && defined(CLOCK_REALTIME)
 
 int tvgettime(Tv_t *tv) {
     struct timespec s;
@@ -30,6 +34,38 @@ int tvgettime(Tv_t *tv) {
     clock_gettime(CLOCK_REALTIME, &s);
     tv->tv_sec = s.tv_sec;
     tv->tv_nsec = s.tv_nsec;
-
     return 0;
 }
+
+#elif _lib_gettimeofday
+
+#include <sys/time.h>
+
+int tvgettime(Tv_t *tv) {
+    struct timeval v;
+
+    gettimeofday(&v, NULL);
+    tv->tv_sec = v.tv_sec;
+    tv->tv_nsec = v.tv_usec * 1000;
+    return 0;
+}
+
+#else
+
+int tvgettime(Tv_t *tv) {
+    time_t s = 0;
+    uint32_t n = 0;
+
+    tv->tv_sec = time(NULL);
+    if (tv->tv_sec != s) {
+        s = tv->tv_sec;
+        n = 0;
+    }
+    else {
+        n += 1000;
+    }
+    tv->tv_nsec = n;
+    return 0;
+}
+
+#endif
