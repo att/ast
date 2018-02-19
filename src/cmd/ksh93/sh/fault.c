411bd00c38f4e0b01e77d4570a108c378427d061
Kurtis Rader <krader@skepticism.us>
2018-02-18 18:50:26 -0800

    Fix `dump_backtrace()` handling of `max_frames` arg

--- src/cmd/ksh93/sh/fault.c
+++ src/cmd/ksh93/sh/fault.c
@@ -60,14 +60,18 @@ static int cursig = -1;
 // that point in the code.
 #if _hdr_execinfo
 void dump_backtrace(int max_frames, int skip_levels) {
+    if (max_frames <= 0) return;
+
     char text[512];
     void *callstack[128];
-    const int n_max_frames = sizeof(callstack) / sizeof(callstack[0]);
+    int n_max_frames = sizeof(callstack) / sizeof(callstack[0]);
     int n_frames = backtrace(callstack, n_max_frames);
     char **symbols = backtrace_symbols(callstack, n_frames);
 
-    int i;
-    for (i = skip_levels; i < n_frames; i++) {
+    max_frames += skip_levels;
+    if (n_frames < max_frames) max_frames = n_frames;
+
+    for (int i = skip_levels; i < max_frames; i++) {
         int n;
         Dl_info info;
         if (dladdr(callstack[i], &info) && info.dli_sname) {
@@ -86,6 +90,8 @@ void dump_backtrace(int max_frames, int skip_levels) {
 #else  // _hdr_execinfo
 
 void dump_backtrace(int max_frames, int skip_levels) {
+    UNUSED(max_frames);
+    UNUSED(skip_levels);
     static char *msg = "Sorry, but dump_backtrace() does not work on your system.\n";
     write(2, msg, strlen(msg));
 }
