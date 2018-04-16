att/ast

commit e3dd5b1a4b1bf58a018bbd9c8626b15113b72717

Fix some use after free cases
These were detected by clang analzyer.
Related: #161

src/cmd/ksh93/bltins/misc.c
@@ -297,6 +297,7 @@ int b_dot_cmd(int n, char *argv[], Shbltin_t *context) {
         }
     }
     sh_popcontext(shp, &buff);
+    nv_putval(SH_PATHNAMENOD, shp->st.filename, NV_NOFREE);
     if (buffer) free(buffer);
     if (!np) free(shp->st.filename);
     shp->dot_depth--;
@@ -310,7 +311,6 @@ int b_dot_cmd(int n, char *argv[], Shbltin_t *context) {
     // Only restore the top Shscope_t portion for posix functions.
     memcpy((void *)&shp->st, (void *)prevscope, sizeof(Shscope_t));
     shp->topscope = (Shscope_t *)prevscope;
-    nv_putval(SH_PATHNAMENOD, shp->st.filename, NV_NOFREE);
     if (jmpval && jmpval != SH_JMPFUN) siglongjmp(*shp->jmplist, jmpval);
     return shp->exitval;
 }

src/cmd/ksh93/edit/history.c
@@ -678,8 +678,9 @@ void hist_flush(History_t *hp) {
             hp->histflush = 0;
         }
         if (sfsync(hp->histfp) < 0) {
+            Shell_t *shp = hp->histshell;
             hist_close(hp);
-            if (!sh_histinit(hp->histshell)) sh_offoption((Shell_t *)hp->histshell, SH_HISTORY);
+            if (!sh_histinit(shp)) sh_offoption((Shell_t *)shp, SH_HISTORY);
         }
         hp->histflush = 0;
     }

src/cmd/ksh93/sh/xec.c
@@ -672,6 +672,7 @@ static void *sh_coinit(Shell_t *shp, char **argv) {
     if (!(csp->coshell = coopen(NULL, CO_SHELL | CO_SILENT, argv[1]))) {
         free(csp);
         errormsg(SH_DICT, ERROR_exit(1), "%s: unable to create namespace", name);
+        __builtin_unreachable();
     }
     csp->coshell->data = (void *)csp;
     csp->name = (char *)(csp + 1);

commit 4ec09f6882314773f044e141af877c2201e78dbd

Correctly restore value of .sh.file after sourcing
Commit e3dd5b1 introduced a regression which was causing .sh.file to be
set to corrupted value after sourcing. This commit reverts part of it.

Resolves: #472

src/cmd/ksh93/bltins/misc.c
@@ -297,7 +297,6 @@ int b_dot_cmd(int n, char *argv[], Shbltin_t *context) {
         }
     }
     sh_popcontext(shp, &buff);
-    nv_putval(SH_PATHNAMENOD, shp->st.filename, NV_NOFREE);
     if (buffer) free(buffer);
     if (!np) free(shp->st.filename);
     shp->dot_depth--;
@@ -311,6 +310,7 @@ int b_dot_cmd(int n, char *argv[], Shbltin_t *context) {
     // Only restore the top Shscope_t portion for posix functions.
     memcpy((void *)&shp->st, (void *)prevscope, sizeof(Shscope_t));
     shp->topscope = (Shscope_t *)prevscope;
+    nv_putval(SH_PATHNAMENOD, shp->st.filename, NV_NOFREE);
     if (jmpval && jmpval != SH_JMPFUN) siglongjmp(*shp->jmplist, jmpval);
     return shp->exitval;
 }