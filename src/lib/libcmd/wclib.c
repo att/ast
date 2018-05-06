77e9cb037630c1eec46b1cea7eb6518155c508a9
Kurtis Rader <krader@skepticism.us>
2018-05-05 17:06:22 -0700

    Remove "#pragma prototyped" recently introduced

--- src/lib/libcmd/wclib.c
+++ src/lib/libcmd/wclib.c
@@ -18,7 +18,6 @@
 *                  David Korn <dgk@research.att.com>                   *
 *                                                                      *
 ***********************************************************************/
-#pragma prototyped
 /*
  * David Korn
  * AT&T Bell Laboratories
