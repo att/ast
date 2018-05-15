0ad6f36217c73aca8023548cfb9b591ac027e9c8
Siteshwar Vashisht <svashisht@redhat.com>
2018-05-15 19:49:18 +0200

    Add configurations for coverity scan
    Coverity scan can be triggered by executing trigger-coverity-scan.sh
    script.
    
    Related: #327

---
+++ bin/trigger-coverity-scan.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Get current branch name
+current=$(git rev-parse --abbrev-ref HEAD | tr -d "\n")
+# Coverity scan is triggered only for coverity_scan branch
+git branch -D coverity_scan
+git checkout -b coverity_scan
+# Use custom .travis.yml file for coverity
+cp coverity-travis.yml .travis.yml
+git commit -a -m "Trigger coverity scan"
+git push --force -u origin coverity_scan
+# Switch back to previous branch
+git checkout $current
