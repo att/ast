09a70bbcf57022aabed950ced0e959795a89ff68
Siteshwar Vashisht <svashisht@redhat.com>
2018-05-17 16:20:16 +0200

    Restyle code for libcoshell/colib.h

--- src/lib/libcoshell/colib.h
+++ src/lib/libcoshell/colib.h
@@ -17,12 +17,12 @@
  *               Glenn Fowler <glenn.s.fowler@gmail.com>                *
  *                                                                      *
  ***********************************************************************/
-/*
- * Glenn Fowler
- * AT&T Research
- *
- * coshell library private definitions
- */
+//
+// Glenn Fowler
+// AT&T Research
+//
+// coshell library private definitions
+//
 
 #ifndef _COLIB_H
 #define _COLIB_H
@@ -32,38 +32,38 @@
 #include <vmalloc.h>
 
 #define _CO_JOB_PRIVATE_  /* Cojob_t private additions	*/ \
-    Cojob_t *next;        /* next in list			*/            \
-    Coservice_t *service; /* service 			*/                \
+    Cojob_t *next;        /* Next in list			*/            \
+    Coservice_t *service; /* Service 			*/                \
     int pid;              /* pid				*/                    \
-    char *out;            /* serialized stdout file	*/    \
-    char *err;            /* serialized stderr file	*/    \
-                          /* end of private additions	*/
+    char *out;            /* Serialized stdout file	*/    \
+    char *err;            /* Serialized stderr file	*/    \
+                          /* End of private additions	*/
 
 #define _CO_SHELL_PRIVATE_ /* Coshell_t private additions	*/  \
     Vmalloc_t *vm;         /* Coshell_t vm			*/               \
-    Coshell_t *next;       /* next in list			*/               \
-    Cojob_t *jobs;         /* job list			*/                   \
-    Coservice_t *service;  /* service 			*/                   \
+    Coshell_t *next;       /* Next in list			*/               \
+    Cojob_t *jobs;         /* Job list			*/                   \
+    Coservice_t *service;  /* Service 			*/                   \
     Dt_t *export;          /* coexport() dictionary	*/        \
     Dtdisc_t *exdisc;      /* coexport() discipline	*/        \
-    struct Coinit_s        /* initialization script state	*/  \
+    struct Coinit_s        /* Initialization script state	*/  \
     {                                                         \
-        char *script;  /* initialization script	*/            \
-        dev_t pwd_dev; /* previous pwd dev		*/                \
-        ino_t pwd_ino; /* previous pwd inode number	*/        \
-        int mask;      /* previous umask		*/                  \
-        int sync;      /* sync script			*/                    \
+        char *script;  /* Initialization script	*/            \
+        dev_t pwd_dev; /* Previous pwd dev		*/                \
+        ino_t pwd_ino; /* Previous pwd inode number	*/        \
+        int mask;      /* Previous umask		*/                  \
+        int sync;      /* Sync script			*/                    \
     } init;                                                   \
-    int cmdfd;           /* command pipe fd		*/               \
+    int cmdfd;           /* Command pipe fd		*/               \
     int gsmfd;           /* msgfp child write side	*/         \
     int mask;            /* CO_* flags to clear		*/           \
-    int mode;            /* connection modes		*/              \
-    int svc_outstanding; /* outstanding service intercepts */ \
-    int svc_running;     /* running service intercepts	*/     \
+    int mode;            /* Connection modes		*/              \
+    int svc_outstanding; /* Outstanding service intercepts */ \
+    int svc_running;     /* Running service intercepts	*/     \
     int pid;             /* pid				*/                         \
     int index;           /* coshell index		*/                 \
-    int slots;           /* number of job slots		*/           \
-                         /* end of private additions	*/
+    int slots;           /* Number of job slots		*/           \
+                         /* End of private additions	*/
 
 typedef struct Coexport_s {
     Dtlink_t link;
@@ -74,15 +74,15 @@ typedef struct Coexport_s {
 struct Coservice_s;
 typedef struct Coservice_s Coservice_t;
 
-struct Coservice_s /* service info			*/
+struct Coservice_s // Service info
 {
-    Coservice_t *next; /* next in list			*/
-    char *name;        /* instance name		*/
-    char *path;        /* coexec() command path	*/
-    char *db;          /* state/db path		*/
-    int fd;            /* command pipe			*/
-    int pid;           /* pid				*/
-    char *argv[16];    /* coexec() command argv[]	*/
+    Coservice_t *next; // Next in list
+    char *name;        // Instance name
+    char *path;        // coexec() command path
+    char *db;          // State/db path
+    int fd;            // Command pipe
+    int pid;           // Pid
+    char *argv[16];    // coexec() command argv[]
 };
 
 #include <coshell.h>
@@ -90,40 +90,40 @@ struct Coservice_s /* service info			*/
 #include <sig.h>
 #include <wait.h>
 
-#define state _coshell_info_ /* hide external symbol		*/
+#define state _coshell_info_ // Hide external symbol
 
-#define CO_MODE_ACK (1 << 0)      /* wait for coexec() ack	*/
-#define CO_MODE_INDIRECT (1 << 1) /* indirect CO_SERVER		*/
-#define CO_MODE_SEPARATE (1 << 2) /* 1 shell+wait per action	*/
+#define CO_MODE_ACK (1 << 0)      // Wait for coexec() ack
+#define CO_MODE_INDIRECT (1 << 1) // Indirect CO_SERVER
+#define CO_MODE_SEPARATE (1 << 2) // 1 shell+wait per action
 
-#define CO_INIT (CO_USER >> 1) /* initial command		*/
+#define CO_INIT (CO_USER >> 1) // Initial command
 
-#define CO_PID_FREE (-3)   /* free job slot		*/
-#define CO_PID_WARPED (-2) /* exit before start message	*/
-#define CO_PID_ZOMBIE (-1) /* ready for wait		*/
+#define CO_PID_FREE (-3)   // Free job slot
+#define CO_PID_WARPED (-2) // Exit before start message
+#define CO_PID_ZOMBIE (-1) // Ready for wait
 
-#define CO_BUFSIZ (PATH_MAX / 2)  /* temporary buffer size	*/
-#define CO_MAXEVAL (PATH_MAX * 8) /* max eval'd action size	*/
+#define CO_BUFSIZ (PATH_MAX / 2)  // Temporary buffer size
+#define CO_MAXEVAL (PATH_MAX * 8) // Max eval'd action size
 
-typedef struct Costate_s /* global coshell state		*/
+typedef struct Costate_s // Global coshell state
 {
-    const char *lib;     /* library id			*/
-    Coshell_t *coshells; /* list of all coshells		*/
-    Coshell_t *current;  /* current coshell		*/
-    Coshell_t *generic;  /* generic coshell for coinit()	*/
-    char *pwd;           /* pwd				*/
-    char *sh;            /* sh from first coopen()	*/
-    char *type;          /* CO_ENV_TYPE value		*/
-    int init;            /* 0 if first coopen()		*/
-    int index;           /* last coshell index		*/
+    const char *lib;     // Library id
+    Coshell_t *coshells; // List of all coshells
+    Coshell_t *current;  // Current coshell
+    Coshell_t *generic;  // Generic coshell for coinit()
+    char *pwd;           // pwd
+    char *sh;            // sh from first coopen()
+    char *type;          // CO_ENV_TYPE value
+    int init;            // 0 if first coopen()
+    int index;           // Last coshell index
 } Costate_t;
 
-extern char coident[];    /* coshell ident script		*/
-extern char cobinit[];    /* bsh initialition script	*/
-extern char cokinit[];    /* ksh initialition script	*/
-extern char *co_export[]; /* default export var list	*/
+extern char coident[];    // Coshell ident script
+extern char cobinit[];    // bsh initialition script
+extern char cokinit[];    // ksh initialition script
+extern char *co_export[]; // Default export var list
 
-extern Costate_t state; /* global coshell info		*/
+extern Costate_t state; // Global coshell info
 
 extern char *costash(Sfio_t *);
 extern char *coinitialize(Coshell_t *, int);
