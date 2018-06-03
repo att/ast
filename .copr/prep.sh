eada2dce58bf7c27c406d4fbebdb11b1de070801
Siteshwar Vashisht <svashisht@redhat.com>
2018-06-03 15:16:39 +0200

    Set correct version number in copr and OBS builds
    Use fallback-version-number option to set correct version number string
    in copr and OBS builds.
    
    Resolves: #570

--- .copr/prep.sh
+++ .copr/prep.sh
@@ -8,15 +8,20 @@ set -x
 # repo, so ignore if it fails.
 git fetch --unshallow || :
 
+# Fetch tags to determine version number
+git fetch --tags
+
 COMMIT=$(git rev-parse HEAD)
 COMMIT_SHORT=$(git rev-parse --short HEAD)
 COMMIT_NUM=$(git rev-list HEAD --count)
 COMMIT_DATE=$(date --date="@$(git show -s --format=%ct HEAD)" +%Y%m%d)
+VCS_VERSION=$(git describe --always --dirty --tags)
 
 sed "s,#COMMIT#,${COMMIT},;
      s,#SHORTCOMMIT#,${COMMIT_SHORT},;
      s,#COMMITNUM#,${COMMIT_NUM},;
-     s,#COMMITDATE#,${COMMIT_DATE}," \
+     s,#COMMITDATE#,${COMMIT_DATE},;
+     s,#VCS_VERSION#,${VCS_VERSION}," \
          packaging/fedora/ksh.spec.in > packaging/fedora/ksh.spec
 
 git archive --prefix "ast-${COMMIT}/" --format "tar.gz" HEAD -o "packaging/fedora/ksh-${COMMIT_SHORT}.tar.gz"
