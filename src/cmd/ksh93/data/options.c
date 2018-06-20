b27eaacac83639616c31b61d217215cb64d37a3d
Siteshwar Vashisht <svashisht@redhat.com>
2018-06-20 12:38:17 +0200

    Define bash builtins only if SHOPT_BASH is set
    Otherwise empty elements are created in shtab_options variable which
    was causing crashes.
    
    Related: #238

--- src/cmd/ksh93/data/options.c
+++ src/cmd/ksh93/data/options.c
@@ -42,68 +42,105 @@ const Shtable_t shtab_options[] = {{"allexport", SH_ALLEXPORT},
 #endif  // SHOPT_BASH
                                    {"bgnice", SH_BGNICE},
                                    {"braceexpand", SH_BRACEEXPAND},
+#if SHOPT_BASH
                                    {bashopt("nocaseglob", SH_NOCASEGLOB)},
                                    {bashopt("cdable_vars", SH_CDABLE_VARS)},
                                    {bashopt("cdspell", SH_CDSPELL)},
                                    {bashopt("checkhash", SH_CHECKHASH)},
                                    {bashopt("checkwinsize", SH_CHECKWINSIZE)},
+#endif  // SHOPT_BASH
                                    {"noclobber", SH_NOCLOBBER},
+
+#if SHOPT_BASH
                                    {bashopt("dotglob", SH_DOTGLOB)},
+#endif  // SHOPT_BASH
                                    {"emacs", SH_EMACS},
+
+#if SHOPT_BASH
                                    {bashopt("empty_cmd_completion", SH_NOEMPTYCMDCOMPL)},
+#endif  // SHOPT_BASH
                                    {"errexit", SH_ERREXIT},
                                    {"noexec", SH_NOEXEC},
+#if SHOPT_BASH
                                    {bashopt("execfail", SH_EXECFAIL)},
                                    {bashopt("expand_aliases", SH_EXPAND_ALIASES)},
                                    {bashopt("extglob", SH_EXTGLOB)},
+#endif  // SHOPT_BASH
                                    {"noglob", SH_NOGLOB},
                                    {"globstar", SH_GLOBSTARS},
                                    {"gmacs", SH_GMACS},
+#if SHOPT_BASH
                                    {bashextra("hashall", SH_TRACKALL)},
                                    {bashopt("histappend", SH_HISTAPPEND)},
+
+#endif  // SHOPT_BASH
                                    {"histexpand", SH_HISTEXPAND},
+#if SHOPT_BASH
                                    {bashextra("history", SH_HISTORY2)},
                                    {bashopt("histreedit", SH_HISTREEDIT)},
                                    {bashopt("histverify", SH_HISTVERIFY)},
                                    {bashopt("hostcomplete", SH_HOSTCOMPLETE)},
                                    {bashopt("huponexit", SH_HUPONEXIT)},
+
+#endif  // SHOPT_BASH
                                    {"ignoreeof", SH_IGNOREEOF},
                                    {"interactive", SH_INTERACTIVE | SH_COMMANDLINE},
+#if SHOPT_BASH
                                    {bashextra("interactive_comments", SH_INTERACTIVE_COMM)},
+
+#endif  // SHOPT_BASH
                                    {"keyword", SH_KEYWORD},
                                    {"letoctal", SH_LETOCTAL},
+#if SHOPT_BASH
                                    {bashopt("lastpipe", SH_LASTPIPE)},
                                    {bashopt("lithist", SH_LITHIST)},
+#endif  // SHOPT_BASH
                                    {"nolog", SH_NOLOG},
                                    {"login_shell", SH_LOGIN_SHELL | SH_COMMANDLINE},
+#if SHOPT_BASH
                                    {bashopt("mailwarn", SH_MAILWARN)},
+#endif  // SHOPT_BASH
                                    {"markdirs", SH_MARKDIRS},
                                    {"monitor", SH_MONITOR},
                                    {"multiline", SH_MULTILINE},
                                    {"notify", SH_NOTIFY},
+
+#if SHOPT_BASH
                                    {bashopt("nullglob", SH_NULLGLOB)},
                                    {bashextra("onecmd", SH_TFLAG)},
+#endif  // SHOPT_BASH
                                    {"pipefail", SH_PIPEFAIL},
+#if SHOPT_BASH
                                    {bashextra("physical", SH_PHYSICAL)},
                                    {bashextra("posix", SH_POSIX)},
+#endif  // SHOPT_BASH
                                    {"privileged", SH_PRIVILEGED},
 #if SHOPT_BASH
                                    {"profile", SH_LOGIN_SHELL | SH_COMMANDLINE},
-#endif  // SHOPT_BASH
                                    {bashopt("progcomp", SH_PROGCOMP)},
                                    {bashopt("promptvars", SH_PROMPTVARS)},
+#endif  // SHOPT_BASH
                                    {"rc", SH_RC | SH_COMMANDLINE},
                                    {"restricted", SH_RESTRICTED},
+#if SHOPT_BASH
                                    {bashopt("restricted_shell", SH_RESTRICTED2 | SH_COMMANDLINE)},
                                    {bashopt("shift_verbose", SH_SHIFT_VERBOSE)},
+
+#endif  // SHOPT_BASH
                                    {"showme", SH_SHOWME},
+#if SHOPT_BASH
                                    {bashopt("sourcepath", SH_SOURCEPATH)},
+
+#endif  // SHOPT_BASH
                                    {"trackall", SH_TRACKALL},
                                    {"nounset", SH_NOUNSET},
                                    {"verbose", SH_VERBOSE},
                                    {"vi", SH_VI},
                                    {"viraw", SH_VIRAW},
+
+#if SHOPT_BASH
                                    {bashopt("xpg_echo", SH_XPG_ECHO)},
+#endif  // SHOPT_BASH
                                    {"xtrace", SH_XTRACE},
                                    {"", 0}};
 
