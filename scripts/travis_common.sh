cd1dd100046fb12fcf9cc29574efa27bc1338531
Kurtis Rader <krader@skepticism.us>
2018-06-23 22:30:32 -0700

    On Travis macOS some compiler flags aren't valid

--- scripts/travis_common.sh
+++ scripts/travis_common.sh
@@ -5,7 +5,13 @@
 export LANG=en_US.UTF-8
 export CFLAGS="-fno-strict-aliasing -Wno-unknown-pragmas -Wno-missing-braces"
 CFLAGS="$CFLAGS -Wno-unused-result -Wno-return-type -Wno-int-to-pointer-cast"
-CFLAGS="$CFLAGS -Wno-parentheses -Wno-unused -Wno-unused-but-set-variable -Wno-cpp"
+CFLAGS="$CFLAGS -Wno-parentheses -Wno-unused"
+if [ "$DISTRO_TYPE" != "macOS" ]
+then
+    # At this time the clang compiler in the Travis macOS environment does not
+    # support these compiler flags. So include them only if not on macOS.
+    CFLAGS="$CFLAGS -Wno-unused-but-set-variable -Wno-cpp"
+fi
 
 # Removing all the TRAVIS_*, NodeJS, Ruby and other irrelevant env vars. This
 # makes a huge difference in the size of the unit test logfile.
