cbf55e73c335100a594de694b37e38020ee50aad
Siteshwar Vashisht <svashisht@redhat.com>
2018-06-26 15:15:06 +0200

    Add script to trigger application compatibility tests
    Resolves: #598

---
+++ bin/trigger-app-compat-tests.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Get current branch name
+current=$(git rev-parse --abbrev-ref HEAD | tr -d "\n")
+git branch -D application_compatibility 2>/dev/null
+git checkout -b application_compatibility
+# Use custom .travis.yml file
+cp app-compat-travis.yml .travis.yml
+git commit -a -m "Trigger application compatibility tests"
+git push --force -u origin application_compatibility
+# Switch back to previous branch
+git checkout $current
