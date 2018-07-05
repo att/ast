8b2cea103802cd4c65476661c25755a67198680b
Kurtis Rader <krader@skepticism.us>
2018-07-05 12:33:09 -0700

    FreeBSD defines NSIG with incorrect value
    On some systems, such as FreeBSD, NSIG is incorrect but the platform does
    define SIGRTMAX so check that first.

--- features/max_signum.c
+++ features/max_signum.c
@@ -1,8 +1,14 @@
 // This is used during the Meson config step to detect the largest signal number.
+// On some systems, such as FreeBSD, NSIG is incorrect but the platform does
+// define SIGRTMAX so check that first.
 #include <signal.h>
 #include <stdio.h>
 
 int main() {
+#ifdef SIGRTMAX
+    printf("%d\n", SIGRTMAX);
+    return 0;
+#else  // SIGRTMAX
 #ifdef _NSIG
     printf("%d\n", _NSIG - 1);
     return 0;
@@ -11,12 +17,8 @@ int main() {
     printf("%d\n", NSIG - 1);
     return 0;
 #else  // NSIG
-#ifdef SIGRTMAX
-    printf("%d\n", SIGRTMAX);
-    return 0;
-#else  // SIGRTMAX
     return 1;
-#endif  // SIGRTMAX
 #endif  // NSIG
 #endif  // _NSIG
+#endif  // SIGRTMAX
 }
