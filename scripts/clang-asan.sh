8b620b2a5c2618fdc3e341ea408d0d14079b8900
Kurtis Rader <krader@skepticism.us>
2018-07-11 23:23:36 -0700

    Enable ASAN testing with clang on macOS
    This makes it possible to build with ASAN enabled using clang on macOS
    and FreeBSD. However, the resulting ksh binary fails to run on FreeBSD
    due to the `getenv()` intercept which interferes with ASAN
    initialization. TBD is what to do about that.

---
+++ scripts/clang-asan.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+#
+# Helper script to determine a couple of options needed when building with
+# clang and ASAN enabled.
+#
+rpath_arg=no
+
+for arg in $( clang -fsanitize=address -v -o /tmp/clang$$.out etc/hdrs.c 2>&1 | tail -1 )
+do
+    if [ $rpath_arg == yes ]
+    then
+        rpath_arg=no
+        echo "$arg"
+    else
+        case "$arg" in
+            -rpath)
+                rpath_arg=yes
+                echo "$arg"
+                ;;
+            -lpthread)
+                echo "$arg"
+                ;;
+            -L*)
+                echo "$arg"
+                ;;
+            *libclang_rt.asan*)
+                echo "$arg"
+                ;;
+        esac
+    fi
+done
+rm /tmp/clang$$.out
