474c60f75ffbd96a0be972dea07b841d27381282
Kurtis Rader <krader@skepticism.us>
2018-07-25 18:01:57 -0700

    Cleanup ast_assert.h

--- src/lib/libast/include/ast_assert.h
+++ src/lib/libast/include/ast_assert.h
@@ -1,8 +1,7 @@
 //
-// We have our own private `assert()` implementation because too many of the platform implements
-// result in lint warnings from tools like cppcheck. They also tend to write the error to stdout
+// We have our own private `assert()` implementation because too many platform implementations
+// cause lint warnings from tools like cppcheck. They also tend to write the error to stdout
 // rather than stderr which is wrong.
-//
 //
 #ifndef _AST_ASSERT_H
 #define _AST_ASSERT_H 1
@@ -16,7 +15,9 @@
 #else  // NDEBUG
 #define assert(e) ((void)((e) ? ((void)0) : __assert(#e, __FILE__, __LINE__)))
 #define __assert(e, file, line) \
-    ((void)fprintf(stderr, "%s:%d: failed assertion '%s'\n", file, line, e), abort())
+    ((void)fprintf(stderr, "%s:%d: failed assertion '%s'\n", file, line, e), \
+     (void)fflush(stderr), \
+     abort())
 #endif  // NDEBUG
 
 #endif  // _AST_ASSERT_H
