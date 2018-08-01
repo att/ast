att/ast

commit 17f2fe2caa809e7aa7f27746048fc8f231d7111a

Keep exit status consistent for non-executable commands
POSIX has 2 different statements about searching commands in PATH:

If a command is not found, the exit status shall be 127. If the command
name is found, but it is not an executable utility, the exit status
shall be 126. Applications that invoke utilities without using the shell
should use these exit status values to report similar errors.

and

The list shall be searched from beginning to end, applying the filename
to each prefix, until an executable file with the specified name and
appropriate execution permissions is found.

It means that shell should not stop searching if a command is found, but
not executable. And it should return 126 only when executable command is
not present in other elements of PATH.

Ksh already complies with this behavior. However if a command is found,
but not executable, it was returning with wrong exit code in some cases:

1. If a command is found in the last element of PATH but is not
executable, ksh returns with status 126.

2. If a command is found before last element of PATH but is not
executable, ksh continues to search for it further in other paths. It
returns with status 127 when executable command could not be found in
other paths.

It should behave consistently and return with status 126 when a command
is found in PATH but is not executable.

Resolves: #485

src/cmd/ksh93/sh/path.c
@@ -876,6 +876,7 @@ void path_exec(Shell_t *shp, const char *arg0, char *argv[], struct argnod *loca
     const char *opath;
     Pathcomp_t *libpath, *pp = 0;
     int slash = 0;
+    int not_executable = 0;
 
     sh_setlist(shp, local, NV_EXPORT | NV_IDENT | NV_ASSIGN, 0);
     envp = sh_envgen(shp);
@@ -901,14 +902,26 @@ void path_exec(Shell_t *shp, const char *arg0, char *argv[], struct argnod *loca
                 opath = arg0;
             }
             path_spawn(shp, opath, argv, envp, libpath, 0);
+            if ((shp->path_err != ENOENT) && (shp->path_err != EACCES)) {
+                // An executable command was found, but failed to execute it
+                errormsg(SH_DICT, ERROR_system(ERROR_NOEXEC), e_exec, arg0);
+            } else if (shp->path_err == EACCES) {
+                // A command was found but it was not executable.
+                // POSIX specifies that shell should continue to search for command in PATH
+                // and return 126 only when it can not find executable file in other elements
+                // of PATH.
+                not_executable = 1;
+            }
             while (pp && (pp->flags & PATH_FPATH)) pp = path_nextcomp(shp, pp, arg0, 0);
         } while (pp);
     // Force an exit.
     ((struct checkpt *)shp->jmplist)->mode = SH_JMPEXIT;
-    if ((errno = shp->path_err) == ENOENT) {
-        errormsg(SH_DICT, ERROR_exit(ERROR_NOENT), e_found, arg0);
-    } else {
+    if (not_executable) {
+        // This will return with status 126.
         errormsg(SH_DICT, ERROR_system(ERROR_NOEXEC), e_exec, arg0);
+    } else {
+        // This will return with status 127.
+        errormsg(SH_DICT, ERROR_exit(ERROR_NOENT), e_found, arg0);
     }
 }
 

src/cmd/ksh93/tests/path.sh
@@ -140,18 +140,15 @@ then
     log_error "leading : in path not working"
 fi
 
-log_info "TODO: Enable this test when bug #485 is fixed."
-# Disabled because once in a while (~20% of the time) the exit status is 126 (ENOENT) rather than
-# the expected 127 (ENOEXEC).
-#
-# (
-#     rm -rf noexec
-#     print 'print cannot execute' > noexec
-#     noexec > /dev/null 2>&1
-# )
-# actual=$?
-# expect=127
-# [[ $actual == $expect ]] || log_error "exit status of non-executable is wrong" "$expect" "$actual"
+# POSIX: If a command is found, but is not executable, exit status should be 126.
+(
+    rm -rf noexec
+    print 'print cannot execute' > noexec
+    noexec > /dev/null 2>&1
+)
+actual=$?
+expect=126
+[[ $actual == $expect ]] || log_error "exit status of non-executable is wrong" "$expect" "$actual"
 
 builtin -d rm 2> /dev/null
 chmod=$(whence chmod)

commit 9446e24f378796eeae060f933ccc5fceda022942

Do not show error if ksh fails to execute a directory
Ksh shows an error if it fails to execute a command that has same name
as a directory that appears under PATH. This is a regression introduced
by commit 17f2fe2caa809e7aa7f27746048fc8f231d7111a and worked as
expected in ksh93u+ release.

Resolves: #757

src/cmd/ksh93/sh/path.c
@@ -925,7 +925,7 @@ void path_exec(Shell_t *shp, const char *arg0, char *argv[], struct argnod *loca
                 opath = arg0;
             }
             path_spawn(shp, opath, argv, envp, libpath, 0);
-            if ((shp->path_err != ENOENT) && (shp->path_err != EACCES)) {
+            if ((shp->path_err != ENOENT) && (shp->path_err != EACCES) && (shp->path_err != EISDIR)) {
                 // An executable command was found, but failed to execute it
                 errormsg(SH_DICT, ERROR_system(ERROR_NOEXEC), e_exec, arg0);
             } else if (shp->path_err == EACCES) {