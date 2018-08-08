att/ast

commit 1dc56d2151541c8f723240f20faf368a9fee02d9

Lint cleanup in src/cmd/ksh93/edit

src/cmd/ksh93/edit/edit.c
@@ -430,10 +430,9 @@ void ed_setup(Edit_t *ep, int fd, int reedit) {
 #ifdef SIGWINCH
     if (shp->winch) {
         int rows = 0, cols = 0;
-        int32_t v;
         astwinsize(2, &rows, &cols);
-        if (v = cols) nv_putval(COLUMNS, (char *)&v, NV_INT32 | NV_RDONLY);
-        if (v = rows) nv_putval(LINES, (char *)&v, NV_INT32 | NV_RDONLY);
+        if (cols) nv_putval(COLUMNS, (char *)&cols, NV_INT32 | NV_RDONLY);
+        if (rows) nv_putval(LINES, (char *)&rows, NV_INT32 | NV_RDONLY);
         shp->winch = 0;
     }
 #endif
@@ -476,7 +475,8 @@ void ed_setup(Edit_t *ep, int fd, int reedit) {
                     int skip = 0;
                     ep->e_crlf = 0;
                     if (pp < ppmax) *pp++ = c;
-                    for (n = 1; c = *last++; n++) {
+                    for (n = 1; *last; n++) {
+                        c = *last++;
                         if (pp < ppmax) *pp++ = c;
                         if (c == '\a' || c == ESC || c == '\r') break;
                         if (skip || (c >= '0' && c <= '9')) {
@@ -577,7 +577,8 @@ void ed_setup(Edit_t *ep, int fd, int reedit) {
             if (r) sh_offoption(shp, SH_RESTRICTED);
             sh_trap(shp, ".sh.subscript=$(tput cuu1 2>/dev/null)", 0);
             if (r) sh_isoption(shp, SH_RESTRICTED);
-            if (pp = nv_getval(SH_SUBSCRNOD)) strncpy(CURSOR_UP, pp, sizeof(CURSOR_UP) - 1);
+            pp = nv_getval(SH_SUBSCRNOD);
+            if (pp) strncpy(CURSOR_UP, pp, sizeof(CURSOR_UP) - 1);
             nv_unset(SH_SUBSCRNOD);
             strcpy(ep->e_termname, term);
         }
@@ -819,7 +820,8 @@ int ed_getchar(Edit_t *ep, int mode) {
                         if (n > 2 || (c != '[' && c != 'O')) break;
                     }
                 }
-                if (n = keytrap(ep, readin, n, LOOKAHEAD - n, mode)) {
+                n = keytrap(ep, readin, n, LOOKAHEAD - n, mode);
+                if (n) {
                     putstack(ep, readin, n, 0);
                     c = ep->e_lbuf[--ep->e_lookahead];
                 } else {
@@ -1013,7 +1015,8 @@ int ed_virt_to_phys(Edit_t *ep, genchar *virt, genchar *phys, int cur, int voff,
 
     sp += voff;
     dp += poff;
-    for (r = poff; c = *sp; sp++) {
+    for (r = poff; *sp; sp++) {
+        c = *sp;
         if (curp == sp) r = dp - phys;
         d = mbwidth((wchar_t)c);
         if (d == 1 && is_cntrl(c)) d = -1;
@@ -1109,7 +1112,7 @@ int ed_external(const genchar *src, char *dest) {
 void ed_gencpy(genchar *dp, const genchar *sp) {
     dp = (genchar *)roundof((char *)dp - (char *)0, sizeof(genchar));
     sp = (const genchar *)roundof((char *)sp - (char *)0, sizeof(genchar));
-    while (*dp++ = *sp++) {
+    while ((*dp++ = *sp++)) {
         ;  // empty loop
     }
 }
@@ -1208,7 +1211,7 @@ static int ed_histlencopy(const char *cp, char *dp) {
     int c, n = 1, col = 1;
     const char *oldcp = cp;
 
-    for (n = 0; c = mbchar(cp); oldcp = cp, col++) {
+    for (n = 0; (c = mbchar(cp)); oldcp = cp, col++) {
         if (c == '\n' && *cp) {
             n += 2;
             if (dp) {
@@ -1296,7 +1299,8 @@ int ed_histgen(Edit_t *ep, const char *pattern) {
         *av = 0;
         strsort(argv, ac, ed_sortdata);
         mplast = (Histmatch_t *)argv[0];
-        for (ar = av = &argv[1]; mp = (Histmatch_t *)*av; av++) {
+        for (ar = av = &argv[1]; *av; av++) {
+            mp = (Histmatch_t *)*av;
             if (strcmp(mp->data, mplast->data) == 0) {
                 mplast->count++;
                 if (mp->index > mplast->index) mplast->index = mp->index;
@@ -1309,7 +1313,11 @@ int ed_histgen(Edit_t *ep, const char *pattern) {
         ac = ar - argv;
         strsort(argv, ac, ed_sortindex);
         mplast = (Histmatch_t *)argv[0];
-        for (av = &argv[1]; mp = (Histmatch_t *)*av; av++, mplast = mp) mplast->next = mp;
+        for (av = &argv[1]; *av; av++) {
+            mp = (Histmatch_t *)*av;
+            mplast->next = mp;
+            mplast = mp;
+        }
         mplast->next = 0;
     }
     ep->hlist = (Histmatch_t **)argv;

src/cmd/ksh93/edit/completion.c
@@ -41,7 +41,7 @@ static char *fmtx(Shell_t *shp, const char *string) {
     }
     if (n == S_EOF && *string != '#') return (char *)string;
     sfwrite(shp->stk, string, --cp - string);
-    for (string = cp; c = mbchar(cp); string = cp) {
+    for (string = cp; (c = mbchar(cp)); string = cp) {
         if ((n = cp - string) == 1) {
             if ((n = state[c]) && n != S_EPAT) sfputc(shp->stk, '\\');
             sfputc(shp->stk, c);
@@ -269,9 +269,8 @@ int ed_expand(Edit_t *ep, char outbuff[], int *cur, int *eol, int mode, int coun
         char *last = out;
         Namval_t *np = nv_search("COMP_KEY", shp->var_tree, 0);
         if (np) np->nvalue.i16 = '\t';
-        if (np = nv_search("COMP_TYPE", shp->var_tree, 0)) {
-            np->nvalue.i16 = (mode == '\\' ? '\t' : '?');
-        }
+        np = nv_search("COMP_TYPE", shp->var_tree, 0);
+        if (np) np->nvalue.i16 = (mode == '\\' ? '\t' : '?');
         c = *(unsigned char *)out;
         var = mode;
         begin = out = find_begin(outbuff, last, 0, &var);
@@ -369,18 +368,17 @@ int ed_expand(Edit_t *ep, char outbuff[], int *cur, int *eol, int mode, int coun
                 *dir = 0;
                 saveout = begin;
             }
-            if (saveout = astconf("PATH_ATTRIBUTES", saveout, NULL)) {
-                nocase = (strchr(saveout, 'c') != 0);
-            }
+            saveout = astconf("PATH_ATTRIBUTES", saveout, NULL);
+            if (saveout) nocase = (strchr(saveout, 'c') != 0);
             if (dir) *dir = c;
             // Just expand until name is unique.
             size += strlen(*com);
         } else {
+            char **tmpcom = com;
             size += narg;
-            {
-                char **savcom = com;
-                while (*com) size += strlen(cp = fmtx(shp, *com++));
-                com = savcom;
+            while (*tmpcom) {
+                cp = fmtx(shp, *tmpcom++);
+                size += strlen(cp);
             }
         }
         // See if room for expansion.
@@ -391,7 +389,8 @@ int ed_expand(Edit_t *ep, char outbuff[], int *cur, int *eol, int mode, int coun
         // Save remainder of the buffer.
         if (*out) left = stkcopy(shp->stk, out);
         if (cmd_completion && mode == '\\') {
-            out = strcopy(begin, path_basename(cp = *com++));
+            cp = *com++;
+            out = strcopy(begin, path_basename(cp));
         } else if (mode == '*') {
             if (ep->e_nlist && dir && var) {
                 if (*cp == var) {

src/cmd/ksh93/edit/emacs.c
@@ -332,7 +332,7 @@ int ed_emacsread(void *context, int fd, char *buff, int scend, int reedit) {
                 ep->mark = i;
                 for (i = eol; i >= cur; i--) out[c + i] = out[i];
                 kptr = kstack;
-                while (i = *kptr++) out[cur++] = i;
+                while ((i = *kptr++)) out[cur++] = i;
                 draw(ep, UPDATE);
                 eol = genlen(out);
                 continue;

src/cmd/ksh93/edit/hexpand.c
@@ -154,7 +154,8 @@ int hist_expand(Shell_t *shp, const char *ln, char **xp) {
     }
 
     // Save shell stack.
-    if (off = stktell(shp->stk)) sp = stkfreeze(shp->stk, 0);
+    off = stktell(shp->stk);
+    if (off) sp = stkfreeze(shp->stk, 0);
 
     cp = (char *)ln;
 
@@ -475,7 +476,8 @@ int hist_expand(Shell_t *shp, const char *ln, char **xp) {
                 c = *++cp;
             }
 
-            if (cc = strchr(modifiers, c)) {
+            cc = strchr(modifiers, c);
+            if (cc) {
                 flag |= mod_flags[cc - modifiers];
             } else {
                 errormsg(SH_DICT, ERROR_ERROR, "%c: unrecognized history modifier", c);
@@ -526,7 +528,8 @@ int hist_expand(Shell_t *shp, const char *ln, char **xp) {
                 flag |= HIST_SUBSTITUTE;
                 while (flag & HIST_SUBSTITUTE) {
                     // Find string.
-                    if (cc = strstr(str, sb.str[0])) {  // replace it
+                    cc = strstr(str, sb.str[0]);
+                    if (cc) {  // replace it
                         c = *cc;
                         *cc = '\0';
                         sfputr(tmp2, str, -1);

src/cmd/ksh93/edit/pcomplete.c
@@ -212,7 +212,8 @@ static void gen_wordlist(Sfio_t *iop, const char *word) {
     char c, n = 0;
 
     while ((c = *word) && strchr(ifs, c)) word++;
-    while (c = *word++) {
+    while (*word) {
+        c = *word++;
         if (strchr(ifs, c)) {
             if (n++) continue;
             c = '\n';
@@ -302,7 +303,8 @@ char **ed_pcomplete(struct Complete *comp, const char *line, const char *prefix,
             cp = (char *)line;
             if (strchr(" \t", *cp)) cp++;
             n = 1;
-            while (c = *cp++) {
+            while (*cp) {
+                c = *cp++;
                 if (strchr(" \t", c)) {
                     if (spaces++ == 0) n++;
                 } else if (spaces) {
@@ -351,8 +353,9 @@ char **ed_pcomplete(struct Complete *comp, const char *line, const char *prefix,
                     cp = nv_getval(np);
                     sfputr(tmp, cp, '\n');
                 } while (nv_nextsub(np));
-            } else if (cp = nv_getval(np)) {
-                sfputr(tmp, cp, '\n');
+            } else {
+                cp = nv_getval(np);
+                if (cp) sfputr(tmp, cp, '\n');
             }
         }
         if (comp->command) {
@@ -374,7 +377,8 @@ char **ed_pcomplete(struct Complete *comp, const char *line, const char *prefix,
     if (comp->suffix) slen = strlen(comp->prefix);
     filter = comp->filter;
     if (comp->options & FILTER_AMP) {
-        while (c = *filter++) {
+        while (*filter) {
+            c = *filter++;
             if (c == '\\' && *filter == '&') {
                 c = *filter++;
             } else if (c == '&') {
@@ -402,7 +406,7 @@ char **ed_pcomplete(struct Complete *comp, const char *line, const char *prefix,
 again:
     c = 0;
     sfseek(tmp, (Sfoff_t)0, SEEK_SET);
-    while (str = sfgetr(tmp, '\n', 0)) {
+    while ((str = sfgetr(tmp, '\n', 0))) {
         wlen = sfvalue(tmp) - 1;
         if (prefix && strncmp(prefix, str, len)) continue;
         if (filter) {
@@ -502,7 +506,8 @@ static const char *lquote(struct Complete *cp, const char *str) {
     stkseek(stakp, 0);
     sfputc(stakp, '$');
     if (sp - str) sfwrite(stakp, str, sp - str);
-    while (c = *sp++) {
+    while (*sp) {
+        c = *sp++;
         if (c == '\'') sfputc(stakp, '\\');
         sfputc(stakp, c);
     }
@@ -519,8 +524,10 @@ static void print_out(struct Complete *cp, Sfio_t *out) {
         if (cp->options & (1 << c)) sfprintf(out, "-o %s ", Option_names[c] + 1);
     }
     while (Actions[c = i++]) {
-        if (a = cp->action & (1L << c)) {
-            if (sp = strchr("abcdefgjksuv", Actions[c])) {
+        a = cp->action & (1L << c);
+        if (a) {
+            sp = strchr("abcdefgjksuv", Actions[c]);
+            if (sp) {
                 sfputc(out, '-');
                 sfputc(out, *sp);
                 sfputc(out, ' ');
@@ -591,8 +598,8 @@ int b_complete(int argc, char *argv[], Shbltin_t *context) {
                 if (Actions[n] == 'c') {
                     // c contains keywords, builtins and funcs.
                     const char *cp;
-                    for (cp = "kbF"; n = *cp; cp++) {
-                        n = (strchr(Actions, n) - Actions);
+                    for (cp = "kbF"; *cp; cp++) {
+                        n = (strchr(Actions, *cp) - Actions);
                         comp.action |= 1L << n;
                     }
                 }
@@ -679,7 +686,8 @@ int b_complete(int argc, char *argv[], Shbltin_t *context) {
             }
         }
         if (empty) argv = av;
-        while (name = *argv++) {
+        while (*argv) {
+            name = *argv++;
             if (print) {
                 if (!compdict || !(cp = (struct Complete *)dtmatch(compdict, name))) {
                     r = 1;

commit 50dde535fa6a39430804b51d8a6a2e592115f37f

read builtin should not wait indefinitely
read builtin waits indefinitely on executing commands like "read foo".
This is a regression introduced by
1dc56d2151541c8f723240f20faf368a9fee02d9 and before this commit
condition in for loop had a side effect even when it's false. This
commit reverts change to this line.

Resolves: #774

src/cmd/ksh93/edit/edit.c
@@ -488,8 +488,7 @@ void ed_setup(Edit_t *ep, int fd, int reedit) {
                     int skip = 0;
                     ep->e_crlf = 0;
                     if (pp < ppmax) *pp++ = c;
-                    for (n = 1; *last; n++) {
-                        c = *last++;
+                    for (n = 1; c = *last++; n++) {
                         if (pp < ppmax) *pp++ = c;
                         if (c == '\a' || c == ESC || c == '\r') break;
                         if (skip || (c >= '0' && c <= '9')) {