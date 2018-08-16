3836e24f7f60b6f0d8b688e2fecb1dbce5dc5306
Kurtis Rader <krader@skepticism.us>
2018-08-15 17:35:38 -0700

    Remove `pvalloc()` feature test
    The function is no longer used.

--- src/lib/libast/include/ast_sys.h
+++ src/lib/libast/include/ast_sys.h
@@ -107,7 +107,6 @@ extern int spawnvex_close(Spawnvex_t *);
 #endif
 
 extern char *fgetcwd(int, char *, size_t);
-extern void *pvalloc(size_t);
 extern char *resolvepath(const char *, char *, size_t);
 #if !_lib_strlcat
 extern size_t strlcat(char *, const char *, size_t);
