1ddfc1804ea409a2f19475853fc8a7f72e636fa8
Kurtis Rader <krader@skepticism.us>
2018-08-28 20:16:44 -0700

    Remove ASTAPI() and _API_ast
    Now that libast is no longer a standalone project we can simplify
    matters and drop the support for old libast APIs.
    
    Resolves #218

--- src/lib/libast/include/ast_api.h
+++ src/lib/libast/include/ast_api.h
@@ -2,51 +2,27 @@
 #define _AST_VERSION AST_VERSION /* pre-20100601 compatibility */
 
 #define AST_VERSION 20131127
-#define ASTAPI(rel) (_BLD_ast || !_API_ast || _API_ast >= rel)
 
-#if !defined(_API_ast) && defined(_API_DEFAULT)
-#define _API_ast _API_DEFAULT
-#endif
-
-#if ASTAPI(20120411)
 #undef cmdopen
 #define cmdopen cmdopen_20120411
-#elif _API_ast >= 20110505
-#undef cmdopen
-#define cmdopen cmdopen_20110505
-#endif
 
-#if ASTAPI(20100601)
 #undef pathaccess
 #define pathaccess pathaccess_20100601
-#endif
 
-#if ASTAPI(20100601)
 #undef pathcat
 #define pathcat pathcat_20100601
-#endif
 
-#if ASTAPI(20100601)
 #undef pathpath
 #define pathpath pathpath_20100601
-#endif
 
-#if ASTAPI(20120528)
 #undef regexec
 #define regexec regexec_20120528
-#endif
 
-#if ASTAPI(20120528)
 #undef regnexec
 #define regnexec regnexec_20120528
-#endif
 
-#if ASTAPI(20120528)
 #undef regrexec
 #define regrexec regrexec_20120528
-#endif
 
-#if ASTAPI(20120528)
 #undef strgrpmatch
 #define strgrpmatch strgrpmatch_20120528
-#endif
