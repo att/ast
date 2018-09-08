c0fc68cc0bb57155adb1f3cd74d56114c425ee6b
Kurtis Rader <krader@skepticism.us>
2018-09-07 21:02:38 -0700

    Lint: prefer early exits and continue

--- src/cmd/ksh93/bltins/typeset.c
+++ src/cmd/ksh93/bltins/typeset.c
@@ -157,63 +157,64 @@ int b_alias(int argc, char *argv[], Shbltin_t *context) {
     troot = tdata.sh->alias_tree;
     if (*argv[0] == 'h') flag = NV_TAGGED;
     if (sh_isoption(tdata.sh, SH_BASH)) tdata.prefix = argv[0];
-    if (argv[1]) {
-        opt_info.offset = 0;
-        opt_info.index = 1;
-        *opt_info.option = 0;
-        tdata.argnum = 0;
-        tdata.aflag = *argv[1];
-        while ((n = optget(argv, sh_optalias))) {
-            switch (n) {
-                case 'p': {
-                    tdata.prefix = argv[0];
-                    break;
-                }
-                case 't': {
-                    flag |= NV_TAGGED;
-                    break;
-                }
-                case 'x': {
-                    flag |= NV_EXPORT;
-                    break;
-                }
-                case ':': {
-                    errormsg(SH_DICT, 2, "%s", opt_info.arg);
-                    break;
-                }
-                case '?': {
-                    errormsg(SH_DICT, ERROR_usage(0), "%s", opt_info.arg);
-                    return 2;
-                }
-            }
-        }
-        if (error_info.errors) {
-            errormsg(SH_DICT, ERROR_usage(2), "%s", optusage(NULL));
-            __builtin_unreachable();
-        }
+    if (!argv[1]) return setall(argv, flag, troot, &tdata);
 
-        argv += (opt_info.index - 1);
-        if (flag & NV_TAGGED) {
-            // Hacks to handle hash -r | --.
-            if (argv[1] && argv[1][0] == '-') {
-                if (argv[1][1] == 'r' && argv[1][2] == 0) {
-                    Namval_t *np = nv_search((char *)PATHNOD, tdata.sh->var_tree, HASH_BUCKET);
-                    nv_putval(np, nv_getval(np), NV_RDONLY);
-                    argv++;
-                    if (!argv[1]) return 0;
-                }
-                if (argv[1][0] == '-') {
-                    if (argv[1][1] == '-' && argv[1][2] == 0) {
-                        argv++;
-                    } else {
-                        errormsg(SH_DICT, ERROR_exit(1), e_option, argv[1]);
-                        __builtin_unreachable();
-                    }
-                }
+    opt_info.offset = 0;
+    opt_info.index = 1;
+    *opt_info.option = 0;
+    tdata.argnum = 0;
+    tdata.aflag = *argv[1];
+    while ((n = optget(argv, sh_optalias))) {
+        switch (n) {
+            case 'p': {
+                tdata.prefix = argv[0];
+                break;
             }
-            troot = tdata.sh->track_tree;
+            case 't': {
+                flag |= NV_TAGGED;
+                break;
+            }
+            case 'x': {
+                flag |= NV_EXPORT;
+                break;
+            }
+            case ':': {
+                errormsg(SH_DICT, 2, "%s", opt_info.arg);
+                break;
+            }
+            case '?': {
+                errormsg(SH_DICT, ERROR_usage(0), "%s", opt_info.arg);
+                return 2;
+            }
         }
     }
+    if (error_info.errors) {
+        errormsg(SH_DICT, ERROR_usage(2), "%s", optusage(NULL));
+        __builtin_unreachable();
+    }
+
+    argv += (opt_info.index - 1);
+    if (flag & NV_TAGGED) {
+        // Hacks to handle hash -r | --.
+        if (argv[1] && argv[1][0] == '-') {
+            if (argv[1][1] == 'r' && argv[1][2] == 0) {
+                Namval_t *np = nv_search((char *)PATHNOD, tdata.sh->var_tree, HASH_BUCKET);
+                nv_putval(np, nv_getval(np), NV_RDONLY);
+                argv++;
+                if (!argv[1]) return 0;
+            }
+            if (argv[1][0] == '-') {
+                if (argv[1][1] == '-' && argv[1][2] == 0) {
+                    argv++;
+                } else {
+                    errormsg(SH_DICT, ERROR_exit(1), e_option, argv[1]);
+                    __builtin_unreachable();
+                }
+            }
+        }
+        troot = tdata.sh->track_tree;
+    }
+
     return setall(argv, flag, troot, &tdata);
 }
 
@@ -1382,8 +1383,8 @@ static_fn void print_scan(Sfio_t *file, int flag, Dt_t *root, bool omit_attrs, s
     char **argv;
     Namval_t *np;
     int namec;
-    Namval_t *onp = 0;
-    char *name = 0;
+    Namval_t *onp = NULL;
+    char *name = NULL;
     size_t len;
 
     tp->sh->last_table = 0;
@@ -1404,29 +1405,30 @@ static_fn void print_scan(Sfio_t *file, int flag, Dt_t *root, bool omit_attrs, s
         sfwrite(file, ":\n", 2);
     } else {
         while (namec--) {
-            if ((np = nv_search(*argv++, root, 0)) && np != onp &&
-                (!nv_isnull(np) || np->nvfun || nv_isattr(np, ~NV_NOFREE))) {
-                onp = np;
-                if (name) {
-                    char *newname = nv_name(np);
-                    if (strncmp(name, newname, len) == 0 && newname[len] == '.') continue;
-                    name = 0;
+            np = nv_search(*argv++, root, 0);
+            if (!np || np == onp) continue;
+            if (nv_isnull(np) && !np->nvfun && !nv_isattr(np, ~NV_NOFREE)) continue;
+
+            onp = np;
+            if (name) {
+                char *newname = nv_name(np);
+                if (strncmp(name, newname, len) == 0 && newname[len] == '.') continue;
+                name = NULL;
+            }
+            if (flag & NV_ARRAY) {
+                if (nv_aindex(np) >= 0) {
+                    if (!(flag & NV_IARRAY)) continue;
+                } else if ((flag & NV_IARRAY)) {
+                    continue;
                 }
-                if (flag & NV_ARRAY) {
-                    if (nv_aindex(np) >= 0) {
-                        if (!(flag & NV_IARRAY)) continue;
-                    } else if ((flag & NV_IARRAY)) {
-                        continue;
-                    }
-                }
-                tp->scanmask = flag & ~NV_NOSCOPE;
-                tp->scanroot = root;
-                tp->sh->last_root = root;
-                print_namval(file, np, omit_attrs, tp);
-                if (!is_abuiltin(np) && nv_isvtree(np)) {
-                    name = nv_name(np);
-                    len = strlen(name);
-                }
+            }
+            tp->scanmask = flag & ~NV_NOSCOPE;
+            tp->scanroot = root;
+            tp->sh->last_root = root;
+            print_namval(file, np, omit_attrs, tp);
+            if (!is_abuiltin(np) && nv_isvtree(np)) {
+                name = nv_name(np);
+                len = strlen(name);
             }
         }
     }
