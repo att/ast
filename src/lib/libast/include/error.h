att/ast

commit ce5dabe06d9cd692ce16182e67d0bdeb72f2e7cd

Remove remaining AST_LC_* symbols
Note that the big block removed from src/cmd/ksh93/sh/init.c is for
building on EBCDIC systems which we no longer support (see #742).

Related #579

src/lib/libast/include/error.h
@@ -50,7 +50,7 @@
 #endif
 
 #ifndef ERROR_translate
-#define ERROR_translating() (error_info.translate && (ast.locale.set & (1 << AST_LC_MESSAGES)))
+#define ERROR_translating() error_info.translate
 #define ERROR_translate(l, i, d, m)                                                           \
     (ERROR_translating()                                                                      \
          ? errorx((const char *)(l), (const char *)(i), (const char *)(d), (const char *)(m)) \

src/cmd/ksh93/sh/init.c
@@ -402,43 +402,6 @@ static_fn void put_lang(Namval_t *np, const void *val, int flags, Namfun_t *fp) 
     }
 
     nv_putv(np, val, flags, fp);
-    if (CC_NATIVE != CC_ASCII && (type == LC_ALL || type == LC_ALL || type == LC_CTYPE)) {
-        if (sh_lexstates[ST_BEGIN] != sh_lexrstates[ST_BEGIN]) free(sh_lexstates[ST_BEGIN]);
-        lctype++;
-        if (ast.locale.set & (1 << AST_LC_CTYPE)) {
-            int c;
-            char *state[4];
-
-            sh_lexstates[ST_BEGIN] = state[0] = (char *)malloc(4 * (1 << CHAR_BIT));
-            memcpy(state[0], sh_lexrstates[ST_BEGIN], (1 << CHAR_BIT));
-            sh_lexstates[ST_NAME] = state[1] = state[0] + (1 << CHAR_BIT);
-            memcpy(state[1], sh_lexrstates[ST_NAME], (1 << CHAR_BIT));
-            sh_lexstates[ST_DOL] = state[2] = state[1] + (1 << CHAR_BIT);
-            memcpy(state[2], sh_lexrstates[ST_DOL], (1 << CHAR_BIT));
-            sh_lexstates[ST_BRACE] = state[3] = state[2] + (1 << CHAR_BIT);
-            memcpy(state[3], sh_lexrstates[ST_BRACE], (1 << CHAR_BIT));
-            for (c = 0; c < (1 << CHAR_BIT); c++) {
-                if (state[0][c] != S_REG) continue;
-                if (state[2][c] != S_ERR) continue;
-                if (isblank(c)) {
-                    state[0][c] = 0;
-                    state[1][c] = S_BREAK;
-                    state[2][c] = S_BREAK;
-                    continue;
-                }
-                if (!isalpha(c)) continue;
-                state[0][c] = S_NAME;
-                if (state[1][c] == S_REG) state[1][c] = 0;
-                state[2][c] = S_ALP;
-                if (state[3][c] == S_ERR) state[3][c] = 0;
-            }
-        } else {
-            sh_lexstates[ST_BEGIN] = (char *)sh_lexrstates[ST_BEGIN];
-            sh_lexstates[ST_NAME] = (char *)sh_lexrstates[ST_NAME];
-            sh_lexstates[ST_DOL] = (char *)sh_lexrstates[ST_DOL];
-            sh_lexstates[ST_BRACE] = (char *)sh_lexrstates[ST_BRACE];
-        }
-    }
 #if ERROR_VERSION < 20000101L
     if (type == LC_ALL || type == LC_MESSAGES) error_info.translate = msg_translate;
 #endif

src/lib/libast/comp/setlocale.c
@@ -58,12 +58,6 @@ extern int mblen(const char *, size_t);
 #undef strxfrm
 #undef valid
 
-#ifndef AST_LC_CANONICAL
-#define AST_LC_CANONICAL LC_abbreviated
-#endif
-
-#define native_locale(a, b, c) ((char *)0)
-
 /*
  * LC_COLLATE and LC_CTYPE native support
  */

src/lib/libast/include/ast_std.h
@@ -64,30 +64,6 @@ extern char *strerror(int);
 
 #define AST_MESSAGE_SET 3 /* see <mc.h> mcindex()		*/
 
-/*
- * maintain this order when adding categories
- */
-
-#define AST_LC_ALL 0
-#define AST_LC_COLLATE 1
-#define AST_LC_CTYPE 2
-#define AST_LC_MESSAGES 3
-#define AST_LC_MONETARY 4
-#define AST_LC_NUMERIC 5
-#define AST_LC_TIME 6
-#define AST_LC_IDENTIFICATION 7
-#define AST_LC_ADDRESS 8
-#define AST_LC_NAME 9
-#define AST_LC_TELEPHONE 10
-#define AST_LC_XLITERATE 11
-#define AST_LC_MEASUREMENT 12
-#define AST_LC_PAPER 13
-#define AST_LC_COUNT 14
-#define AST_LC_LANG 255
-
-#define AST_LC_internal 1
-#define AST_LC_setenv (1L << 27)
-
 #undef strcoll
 #define strcoll _ast_info.collate
 

commit 2ee83294b8ba8ee0d5f86a5dd077f8d204af007a

Fix definition of ERROR_translating macro
There was a regression introduced by changes in ce5dabe and d2aa35f that
deal with removing AST specific locale subsystem. Fix it by using
standard `LC_MESSAGES` symbol.

Resolves: #851

src/lib/libast/include/error.h
@@ -50,7 +50,7 @@
 #endif
 
 #ifndef ERROR_translate
-#define ERROR_translating() error_info.translate
+#define ERROR_translating() (error_info.translate && (ast.locale.set & (1 << LC_MESSAGES)))
 #define ERROR_translate(l, i, d, m)                                                           \
     (ERROR_translating()                                                                      \
          ? errorx((const char *)(l), (const char *)(i), (const char *)(d), (const char *)(m)) \

src/cmd/tests/opt.c
@@ -101,6 +101,7 @@ int main(int argc, char **argv) {
             str = 1;
         } else if (!strcmp(command, "-+")) {
             argv++;
+            ast.locale.set |= (1 << LC_MESSAGES);
             error_info.translate = translate;
         } else if (!strcmp(command, "+") && *(argv + 2)) {
             ext += 2;