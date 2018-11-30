c0c932216668f624cb215421ecd696c06a3cf0d6
Siteshwar Vashisht <svashisht@redhat.com>
2018-11-30 01:02:25 +0100

    Add `sleep` tests for ISO 8601 time format

--- src/cmd/ksh93/tests/sleep.sh
+++ src/cmd/ksh93/tests/sleep.sh
@@ -19,10 +19,12 @@ sleep 0.1
 [[ $SECONDS -ge 0.1 ]] || log_error "sleep 0.1 should sleep for at least 0.1 second"
 
 # ======
-# TODO
 # PnYnMnDTnHnMnS
 # An ISO 8601 duration where at least one of the duration parts must be
 # specified.
+SECONDS=0
+sleep "P0Y0M0DT0H0M1S"
+[[ $SECONDS -ge 1 ]] || log_error "sleep P0Y0M0DT0H0M1S should sleep for at least 1 second"
 
 # ======
 # PnW   An ISO 8601 duration specifying n weeks.
@@ -30,11 +32,13 @@ sleep 0.1
 sleep P0W || log_error "sleep does not recocgnize PnW"
 
 # ======
-# TODO
 # pnYnMnDTnHnmnS
 # A case insensitive ISO 8601 duration except that M specifies months,
 # m before s or S specifies minutes and after specifies milliseconds, u
 # or U specifies microseconds, and n specifies nanoseconds.
+SECONDS=0
+sleep "p0Y0M0DT0H0M1S"
+[[ $SECONDS -ge 1 ]] || log_error "sleep p0Y0M0DT0H0M1S should sleep for at least 1 second"
 
 # ======
 # date/time
