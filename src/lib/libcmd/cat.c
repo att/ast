att/ast

commit e0b7783976ded76ca282e345f4cd11af657d402b

Lint cleanup in src/lib/libcmd

src/lib/libcmd/cat.c
@@ -479,7 +479,8 @@ int b_cat(int argc, char **argv, Shbltin_t *context) {
             }
         }
         if (sferror(sfstdout)) break;
-    } while (cp = *argv++);
+        cp = *argv++;
+    } while (cp);
     if (sfsync(sfstdout)) error(ERROR_system(0), "write error");
     if (flags & d_FLAG) sfopen(sfstdout, NULL, "w");
     return error_info.errors;

src/lib/libcmd/basename.c
@@ -122,6 +122,6 @@ int b_basename(int argc, char **argv, Shbltin_t *context) {
     if (!all)
         namebase(sfstdout, argv[0], argv[1]);
     else
-        while (string = *argv++) namebase(sfstdout, string, suffix);
+        while ((string = *argv++)) namebase(sfstdout, string, suffix);
     return 0;
 }

src/lib/libcmd/cut.c
@@ -612,7 +612,8 @@ int b_cut(int argc, char **argv, Shbltin_t *context) {
             cutcols(cut, fp, sfstdout);
         }
         if (fp != sfstdin) sfclose(fp);
-    } while (cp = *argv++);
+        cp = *argv++;
+    } while (cp);
     if (sfsync(sfstdout)) error(ERROR_system(0), "write error");
     return error_info.errors != 0;
 }

src/lib/libcmd/getconf.c
@@ -227,7 +227,8 @@ int b_getconf(int argc, char **argv, Shbltin_t *context) {
         path = 0;
     } else if (streq(name, empty)) {
         name = NULL;
-        if (path = *++argv) {
+        path = *++argv;
+        if (path) {
             argv++;
             if (streq(path, empty)) {
                 path = 0;

src/lib/libcmd/head.c
@@ -143,7 +143,8 @@ int b_head(int argc, char **argv, Shbltin_t *context) {
         }
     next:
         if (fp != sfstdin) sfclose(fp);
-    } while (cp = *argv++);
+        cp = *argv++;
+    } while (cp);
     if (sfsync(sfstdout)) error(ERROR_system(0), "write error");
     return error_info.errors != 0;
 }

src/lib/libcmd/mkdir.c
@@ -106,7 +106,8 @@ int b_mkdir(int argc, char **argv, Shbltin_t *context) {
         umask(mask);
         mask = 0;
     }
-    while (path = *argv++) {
+    while (*argv) {
+        path = *argv++;
         if (!mkdir(path, mode)) {
             if (vflag) error(0, "%s: directory created", path);
             made = 1;

src/lib/libcmd/stty.c
@@ -623,7 +623,8 @@ static void set(char *argv[], struct termios *sp) {
     int c, off;
     char *cp;
     char *ep;
-    while (cp = *argv++) {
+    while (*argv) {
+        cp = *argv++;
         off = 0;
         if (*cp == '-') {
             cp++;
@@ -708,9 +709,8 @@ static void set(char *argv[], struct termios *sp) {
                 cp = *argv;
                 if (!cp) {
                     if (tp->field == C_SPEED) {
-                        if (tp = getspeed(*tp->name == 'i' ? cfgetispeed(sp) : cfgetospeed(sp))) {
-                            sfprintf(sfstdout, "%s\n", tp->name);
-                        }
+                        tp = getspeed(*tp->name == 'i' ? cfgetispeed(sp) : cfgetospeed(sp));
+                        if (tp) sfprintf(sfstdout, "%s\n", tp->name);
                         break;
                     }
                     error(ERROR_exit(1), "%s: missing numeric argument", tp->name);

src/lib/libcmd/uname.c
@@ -241,7 +241,8 @@ int b_uname(int argc, char **argv, Shbltin_t *context) {
             "CS|SI");
     } else if (*argv) {
         e = &buf[sizeof(buf) - 1];
-        while (s = *argv++) {
+        while (*argv) {
+            s = *argv++;
             t = buf;
             *t++ = 'C';
             *t++ = 'S';
@@ -274,7 +275,8 @@ int b_uname(int argc, char **argv, Shbltin_t *context) {
         if (flags & OPT_implementation) {
             s = astconf("PLATFORM", NULL, NULL);
             if (!(*s) && !*(s = astconf("HW_NAME", NULL, NULL))) {
-                if (t = strchr(hosttype, '.')) {
+                t = strchr(hosttype, '.');
+                if (t) {
                     t++;
                 } else {
                     t = (char *)hosttype;

commit 844a3a6b851141587eafab0df0fd60b20b7327e3

Fix a loop condition in `cat` builtin
`cat` builtin goes in an infinite loop while trying to open a
non-existent file. This is a regression introduced by e0b778397.

Resolves: #1051

src/lib/libcmd/cat.c
@@ -489,8 +489,8 @@ int b_cat(int argc, char **argv, Shbltin_t *context) {
             }
         }
         if (sferror(sfstdout)) break;
-        cp = *argv++;
-    } while (cp);
+        ;
+    } while ((cp = *argv++));
     if (sfsync(sfstdout)) error(ERROR_system(0), "write error");
     if (flags & d_FLAG) sfopen(sfstdout, NULL, "w");
     return error_info.errors;