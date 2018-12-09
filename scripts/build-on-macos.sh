0e72469dbac7ab0736a1c6938e07d5e69ffd1f82
Siteshwar Vashisht <svashisht@redhat.com>
2018-12-09 04:14:19 +0100

    Enable auditing in travis builds

--- scripts/build-on-macos.sh
+++ scripts/build-on-macos.sh
@@ -29,6 +29,9 @@ CORE_COUNT=$(sysctl -n hw.ncpu)
 export MESON_TESTTHREADS=$(( 4 * ${CORE_COUNT:-1} ))
 echo MESON_TESTTHREADS=$MESON_TESTTHREADS
 
+# Enable auditing
+echo "/tmp/ksh_auditfile;$(id -u)" | sudo tee /etc/ksh_audit
+
 # TODO: Run with --setup=malloc when Travis macOS is updated to 10.13 or
 # newer. macOS 10.12 doesn't honor the MallocLogFile=/dev/null env var which
 # results in lots of malloc debug messages being written to stderr which
