c827a1d1f59e79ffe97fb377a417b5408f7f20a5
Siteshwar Vashisht <svashisht@redhat.com>
2019-01-07 09:04:01 +0100

    Add support for `-a` flag in `read` builtin
    Add `-a` flag in `read` builtin to improve compatiblity with bash.
    
    Resolves: #819

--- src/cmd/ksh93/bltins/read.c
+++ src/cmd/ksh93/bltins/read.c
@@ -238,6 +238,11 @@ int b_read(int argc, char *argv[], Shbltin_t *context) {
     }
     while ((r = optget(argv, sh_optread))) {
         switch (r) {
+            case 'a': {
+                // `read -a` is an alias for `read -A`
+                flags |= A_FLAG;
+                break;
+            }
             case 'A': {
                 flags |= A_FLAG;
                 break;
