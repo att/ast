7e38324272324c3ac8814842dc4cb8a7f02660d1
Kurtis Rader <krader@skepticism.us>
2019-02-24 15:40:01 -0800

    Feature test: Detect num cpus
    Several unit tests depend on knowing the number of online CPUs to
    work correctly. If that isn't defined the test may default to a value
    inappropriate for the system resulting in timeouts.
    
    This also removes three CDT tests that depend on Vmalloc features. Those
    tests will never to updated to work with the system malloc.

---
+++ features/detect_nproc.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+#
+# Exit with a status reflecting the CPU count if we can determine it. If not
+# assume two CPUs since at least one test, API/trehash.c, deadlocks if the CPU
+# count is one.
+nproc=2
+if getconf _NPROCESSORS_ONLN >/dev/null 2>&1
+then
+	# Some systems have a getconf that supports this var. Use it in preference
+	# to the options below because those other mechanisms include offline CPUs.
+	nproc=$(getconf _NPROCESSORS_ONLN)
+elif type nproc >/dev/null 2>&1
+then
+	# Linux and macOS but not other BSD.
+	nproc=$(nproc)
+elif sysctl -n hw.ncpu >/dev/null 2>&1
+then
+	# BSD
+	nproc=$(sysctl -n hw.ncpu)
+fi
+
+if [ -z "$nproc" ]
+then
+	nproc=2
+elif [ $nproc -lt 2 ]
+then
+	nproc=2
+elif [ $nproc -gt 64 ]
+then
+	nproc=64
+fi
+exit $nproc
