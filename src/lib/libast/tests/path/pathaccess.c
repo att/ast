5d4e9b3567f4cdc4bc05733ec23e3b6f62d9fe32
Siteshwar Vashisht <svashisht@redhat.com>
2019-03-09 11:41:12 +0100

    Add tests for `pathaccess()` function

---
+++ src/lib/libast/tests/path/pathaccess.c
@@ -0,0 +1,20 @@
+#include "config_ast.h"  // IWYU pragma: keep
+#include "terror.h"
+
+tmain() {
+    UNUSED(argc);
+    UNUSED(argv);
+
+    char path[PATH_MAX];
+
+    const char *paths = "/usr:/usr/bin:/usr/local/bin";
+    pathaccess(paths, 0, 0, 0, path, PATH_MAX);
+
+    if (strcmp(path, "/usr")) terror("pathaccess() failed to resolve path");
+
+    paths = "this_path_does_not_exist";
+    
+    if (pathaccess(paths, 0, 0, PATH_ABSOLUTE, path, PATH_MAX)) terror("pathaccess() returns path to a file that does not exist");
+
+    texit(0);
+}
