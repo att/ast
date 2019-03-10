c81d4951cc45a919b01fe7419715ac3f1f7a400e
kshbugbot <kshbugbot@gmail.com>
2019-03-11 00:15:42 +0100

    [kshbugbot] Fix code styling

--- src/lib/libast/tests/path/pathstat.c
+++ src/lib/libast/tests/path/pathstat.c
@@ -8,30 +8,31 @@
 
 const char *link_to_temp_file = "link_to_temp_file";
 
-void cleanup_handler(void) {
-    unlink(link_to_temp_file);
-}
+void cleanup_handler(void) { unlink(link_to_temp_file); }
 
 tmain() {
     UNUSED(argc);
     UNUSED(argv);
 
-    const char* temp_file = "temp_file";
+    const char *temp_file = "temp_file";
 
     struct stat statbuf;
 
     if (open(temp_file, O_CREAT, 0666) < 0) terror("Failed to create temp file");
 
-    if (pathstat(temp_file,  &statbuf) && statbuf.st_mode == 0666) terror("Failed to stat() %s", temp_file);
+    if (pathstat(temp_file, &statbuf) && statbuf.st_mode == 0666)
+        terror("Failed to stat() %s", temp_file);
 
-    if (symlink(temp_file, link_to_temp_file) < 0) terror("Failed to create symbolic link to %s", temp_file);
+    if (symlink(temp_file, link_to_temp_file) < 0)
+        terror("Failed to create symbolic link to %s", temp_file);
 
     atexit(cleanup_handler);
 
     // Unlink target file to create a broken symbolic link
     unlink(temp_file);
 
-    if (pathstat(link_to_temp_file,  &statbuf)) terror("Failed to stat %s if it's broken", link_to_temp_file);
+    if (pathstat(link_to_temp_file, &statbuf))
+        terror("Failed to stat %s if it's broken", link_to_temp_file);
 
     texit(0);
 }
