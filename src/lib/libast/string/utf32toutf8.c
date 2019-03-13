269f7c482c51822622960687058adf88c714c8f1
Kurtis Rader <krader@skepticism.us>
2019-03-13 16:45:19 -0700

    Simplify utf32toutf8() implementation and add test
    Resolves #1214

--- src/lib/libast/string/utf32toutf8.c
+++ src/lib/libast/string/utf32toutf8.c
@@ -19,61 +19,35 @@
  *                     Phong Vo <phongvo@gmail.com>                     *
  *                                                                      *
  ***********************************************************************/
-/*
- * Glenn Fowler
- * AT&T Research
- *
- * convert utf32 to utf8 in s
- * s must have room for at least UTF8_LEN_MAX bytes
- * return value is the number of chars placed in s
- * thanks Tom Duff
- */
 #include "config_ast.h"  // IWYU pragma: keep
 
 #include <stddef.h>
 #include <stdint.h>
 
-#include "ast.h"
-
-typedef struct Utf8_s {
-    uint32_t range;
-    unsigned short prefix;
-    unsigned short shift;
-} Utf8_t;
-
-static const Utf8_t ops[] = {{0x00000080, 0x00, 0},  {0x00000800, 0xc0, 6},
-                             {0x00010000, 0xe0, 12}, {0x00200000, 0xf0, 18},
-                             {0x04000000, 0xf8, 24}, {0x80000000, 0xfc, 30}};
-
-size_t utf32toutf8(char *s, uint32_t w) {
-    int i;
-    char *b;
-    char tmp[6];
-
-    if (!s) s = tmp;
-    for (i = 0; i < elementsof(ops); i++) {
-        if (w >= ops[i].range) continue;
-        b = s;
-        *s++ = ops[i].prefix | (w >> ops[i].shift);
-        switch (ops[i].shift) {
-            case 30:
-                *s++ = 0x80 | ((w >> 24) & 0x3f);
-            // FALLTHRU
-            case 24:
-                *s++ = 0x80 | ((w >> 18) & 0x3f);
-            // FALLTHRU
-            case 18:
-                *s++ = 0x80 | ((w >> 12) & 0x3f);
-            // FALLTHRU
-            case 12:
-                *s++ = 0x80 | ((w >> 6) & 0x3f);
-            // FALLTHRU
-            case 6:
-                *s++ = 0x80 | (w & 0x3f);
-            // FALLTHRU
-            default:;  // EMPTY BLOCK
-        }
-        return s - b;
+// Convert a Unicode code point to UTF8.
+size_t utf32toutf8(char *buffer, uint32_t code) {
+    if (code <= 0x7F) {
+        buffer[0] = code;
+        return 1;
     }
+    if (code <= 0x7FF) {
+        buffer[0] = 0xC0 | (code >> 6);    // 110xxxxx
+        buffer[1] = 0x80 | (code & 0x3F);  // 10xxxxxx
+        return 2;
+    }
+    if (code <= 0xFFFF) {
+        buffer[0] = 0xE0 | (code >> 12);          // 1110xxxx
+        buffer[1] = 0x80 | ((code >> 6) & 0x3F);  // 10xxxxxx
+        buffer[2] = 0x80 | (code & 0x3F);         // 10xxxxxx
+        return 3;
+    }
+    if (code <= 0x10FFFF) {
+        buffer[0] = 0xF0 | (code >> 18);           // 11110xxx
+        buffer[1] = 0x80 | ((code >> 12) & 0x3F);  // 10xxxxxx
+        buffer[2] = 0x80 | ((code >> 6) & 0x3F);   // 10xxxxxx
+        buffer[3] = 0x80 | (code & 0x3F);          // 10xxxxxx
+        return 4;
+    }
+
     return 0;
 }
