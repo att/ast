53679849ea385296355be5dc8e03b7ca770949c7
Kurtis Rader <krader@skepticism.us>
2019-03-13 21:31:37 -0700

    Coverity Scan issue #253581

--- src/cmd/ksh93/edit/history.c
+++ src/cmd/ksh93/edit/history.c
@@ -927,9 +927,14 @@ static int hist_exceptf(Sfio_t *fp, int type, Sfdisc_t *handle)
         (void)fcntl(oldfd, F_SETFD, FD_CLOEXEC);
 
         close(newfd);
-        if (lseek(oldfd, (off_t)0, SEEK_END) < hp->histcnt) {
+        if (lseek(oldfd, 0, SEEK_END) < hp->histcnt) {
             int index = hp->histind;
-            lseek(oldfd, (off_t)2, SEEK_SET);
+            // The return value of this lseek() has historically been ignored. It is unclear if that
+            // is correct. That is, is there any scenario in which this lseek() could fail but the
+            // overall behavior of the shell still be correct if we ignore that failure? The void
+            // cast is to silence
+            // https://scan4.coverity.com/reports.htm#v37777/p13914/fileInstanceId=57999457&defectInstanceId=10523151&mergedDefectId=253581
+            (void)lseek(oldfd, 2, SEEK_SET);
             hp->histcnt = 2;
             hp->histind = 1;
             hp->histcmds[1] = 2;
