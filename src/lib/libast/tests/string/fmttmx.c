1515d21af6ddc2dc16ae6828139bf65f028687da
Siteshwar Vashisht <svashisht@redhat.com>
2019-03-25 14:49:05 +0100

    Add tests for `fmttmx()` function

---
+++ src/lib/libast/tests/string/fmttmx.c
@@ -0,0 +1,31 @@
+#include "config_ast.h"  // IWYU pragma: keep
+
+#include <string.h>
+
+#include "ast.h"
+#include "terror.h"
+#include "tmx.h"
+
+tmain() {
+    UNUSED(argc);
+    UNUSED(argv);
+
+    // Unix Epoch
+    Time_t t = 0;
+    const char *actual_result;
+
+    const char *expected_result = "Thu Jan  1 00:00:00 UTC 1970";
+
+    // Set timezone to match expected result
+    setenv("TZ", "UTC", 1);
+    tzset();
+
+    // Format strings will be tested through `tmxfmt()` function tests
+    actual_result = fmttmx("", t);
+
+    if (strcmp(actual_result, expected_result)) {
+        terror("fmttmx() failed :: Actual Result : %s, Expected Result : %s", actual_result,
+               expected_result);
+    }
+    texit(0);
+}
