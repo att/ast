1044e8c3f76d9bd46d2cd12d03e12bcd2ab4d405
Siteshwar Vashisht <svashisht@redhat.com>
2019-03-25 12:46:05 +0100

    Add tests for `fmtperm()` function

---
+++ src/lib/libast/tests/string/fmtperm.c
@@ -0,0 +1,30 @@
+#include "config_ast.h"  // IWYU pragma: keep
+
+#include <string.h>
+
+#include "ast.h"
+#include "terror.h"
+
+tmain() {
+    UNUSED(argc);
+    UNUSED(argv);
+
+    const char *actual_result = NULL;
+
+    struct IntegerToStringTest tests[] = {{0777, "u=rwx,g=rwx,o=rwx"},
+                                          {0555, "u=rx,g=rx,o=rx"},
+                                          {0444, "u=r,g=r,o=r"},
+                                          {0755, "u=rwx,g=rx,o=rx"},
+                                          {0, NULL}};
+
+    for (int i = 0; tests[i].input; ++i) {
+        actual_result = fmtperm(tests[i].input);
+
+        if (strcmp(actual_result, tests[i].expected_result)) {
+            terror("fmtperm() failed :: Failed to convert mode to string :: Actual Result : %s, Expected Result : %s",
+                    actual_result, tests[i].expected_result);
+        }
+    }
+
+    texit(0);
+}
