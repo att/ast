fb6cbad3debd7d65b8c5c69977e3587f543b37d8
Siteshwar Vashisht <svashisht@redhat.com>
2019-03-26 13:11:04 +0100

    Add tests for `strlcpy()` function

---
+++ src/lib/libast/tests/string/strlcpy.c
@@ -0,0 +1,24 @@
+#include "config_ast.h"  // IWYU pragma: keep
+
+#include <string.h>
+
+#include "ast.h"
+#include "terror.h"
+
+tmain() {
+    UNUSED(argc);
+    UNUSED(argv);
+
+    char buffer[4] = "foo";
+    char buffer_to_append[] = "barbaz";
+
+    strlcpy(buffer, buffer_to_append, sizeof(buffer));
+
+    if (strcmp(buffer, "bar"))
+        terror(
+            "strlcat() :: Failed to copy buffer with truncation :: Actual Result: %s, Expected "
+            "Result: %s",
+            buffer, "bar");
+
+    texit(0);
+}
