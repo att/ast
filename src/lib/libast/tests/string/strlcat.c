9064f15a49e4d6f54414b66d04a2b115116896ac
Siteshwar Vashisht <svashisht@redhat.com>
2019-03-26 13:07:06 +0100

    Add tests for `strlcat()` function

---
+++ src/lib/libast/tests/string/strlcat.c
@@ -0,0 +1,22 @@
+#include "config_ast.h"  // IWYU pragma: keep
+
+#include <string.h>
+
+#include "ast.h"
+#include "terror.h"
+
+tmain() {
+    UNUSED(argc);
+    UNUSED(argv);
+
+    char buffer[7] = "foo";
+    char buffer_to_append[] = "barbaz";
+
+    strlcat(buffer, buffer_to_append, 7);
+
+    if (strcmp(buffer, "foobar"))
+        terror("strlcat() :: Failed to append buffer :: Actual Result: %s, Expected Result: %s",
+               buffer, "foobar");
+
+    texit(0);
+}
