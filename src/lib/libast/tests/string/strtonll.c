79cf1b35ed6de80e4742048a49a2a0e86baef07f
Siteshwar Vashisht <svashisht@redhat.com>
2019-03-26 16:20:49 +0100

    Add tests for `strton64()` function

---
+++ src/lib/libast/tests/string/strtonll.c
@@ -0,0 +1,29 @@
+#include "config_ast.h"  // IWYU pragma: keep
+
+#include <string.h>
+
+#include "ast.h"
+#include "terror.h"
+
+tmain() {
+    UNUSED(argc);
+    UNUSED(argv);
+
+    int64_t actual_result;
+    char *error;
+
+    struct StringToIntegerTest tests[] = {{"500", 500}, {"999", 999}, {"-123", -123},
+                                          {"0x0f", 15}, {"010", 8},   {NULL, 0}};
+
+    for (int i = 0; tests[i].input; ++i) {
+        actual_result = strton64(tests[i].input, &error, NULL, 1);
+        if (actual_result != tests[i].expected_result) {
+            terror(
+                "strton64 failed() :: Failed to convert string to number :: Actual Result : %d, "
+                "Expected Result : %d",
+                actual_result, tests[i].expected_result);
+        }
+    }
+
+    texit(0);
+}
