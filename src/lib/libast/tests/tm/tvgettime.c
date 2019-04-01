bc86eafce7ebea9429db910bbb132ca20edf71f8
Siteshwar Vashisht <svashisht@redhat.com>
2019-04-01 14:28:20 +0200

    Add test for `tvgetttime()` function

---
+++ src/lib/libast/tests/tm/tvgettime.c
@@ -0,0 +1,24 @@
+#include "config_ast.h"  // IWYU pragma: keep
+
+#include "ast.h"
+#include "terror.h"
+#include "tv.h"
+
+tmain() {
+    UNUSED(argc);
+    UNUSED(argv);
+
+    Tv_t tv;
+    struct timeval timeval;
+
+    tvgettime(&tv);
+
+    gettimeofday(&timeval, NULL);
+
+    // If time difference is greater than 1 second, treat it as an error.
+    if (timeval.tv_sec - tv.tv_sec > 1) {
+        terror("tvgetime() failed :: tvgetime() failed to get current time");
+    }
+
+    texit(0);
+}
