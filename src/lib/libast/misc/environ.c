180bc56590b6da5f3ac0c64f294b9b57020b61db
Siteshwar Vashisht <svashisht@redhat.com>
2019-05-03 12:08:09 +0200

    Remove stale comment about enviornment related functions

--- src/lib/libast/misc/environ.c
+++ src/lib/libast/misc/environ.c
@@ -27,17 +27,6 @@
 #include "ast.h"
 #include "ast_assert.h"
 
-// This exists solely for libast unit tests and standalone commands (e.g., pty) which don't link
-// against the ksh code. It emulates the behavior of the ksh functions of the same name. As of
-// ksh93v- and earlier releases this was much more complex with a dispatch table. The intent was to
-// allow code that used the `getenv()` and `setenviron()` functions to work regardless of whether or
-// not the ksh code was linked into the binary. It also supported, at least in theory if not
-// practice, other code (e.g., ksh plugins and functions from system libraries) to call `getenv()`
-// and get the ksh version.
-
-// TODO: See if this module can be removed. For example, by turning the relevant ksh code into a
-// library that is also linked with the libast unit tests.
-
 #undef getenv
 extern char *getenv(const char *name);
 char *sh_getenv(const char *name) { return getenv(name); }
