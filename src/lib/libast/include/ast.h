att/ast

commit 9755dceb5adcb77cc28ee263108d7b09056f4d78

Eliminate astconf("PATH")
Calling astconf("PATH") is just a thin wrapper around confstr(_CS_PATH).
So in order to eliminate three more uses of astconf() replace those calls
with a simpler wrapper around confstr().

src/lib/libast/include/ast.h
@@ -208,6 +208,7 @@ extern char *fmtre(const char *);
 extern char *fmtscale(Sfulong_t, int);
 extern void *memdup(const void *, size_t);
 extern char *pathaccess(const char *, const char *, const char *, int, char *, size_t);
+extern const char *cs_path(void);
 extern char *pathbin(void);
 extern char *pathcanon(char *, size_t, int);
 extern char *pathcat(const char *, int, const char *, const char *, char *, size_t);

src/lib/libast/path/pathbin.c
@@ -26,27 +26,33 @@
 #include "config_ast.h"  // IWYU pragma: keep
 
 #include <string.h>
+#include <unistd.h>
 
 #include "ast.h"
 
+static const char *def_cs_path = "/bin:/usr/bin:/usr/local/bin";
+static char *saved_cs_path = NULL;
+#define CS_PATH_LEN 1024
+
+//
+//  Return default PATH to find all system utilities.
+//
+const char *cs_path() {
+    if (!saved_cs_path) {
+        saved_cs_path = malloc(CS_PATH_LEN);
+        size_t rv = confstr(_CS_PATH, saved_cs_path, CS_PATH_LEN);
+        if (rv == 0) strlcpy(saved_cs_path, def_cs_path, CS_PATH_LEN);
+        // Make sure the string is null terminated in case the buf was too small.
+        saved_cs_path[CS_PATH_LEN - 1] = '\0';
+    }
+    return saved_cs_path;
+}
+
+//
 //  Return current PATH
 //
 char *pathbin(void) {
-    char *path;
-    // Used to restore value of saved astconf path from earlier function call
-    static char *astconf_path = NULL;
-
-    path = getenv("PATH");
-    if (!path || !*path) path = astconf_path;
-
-    if (!path) {
-        path = astconf("PATH", NULL, NULL);
-        if (path && *path) path = strdup(path);
-        if (!path) {
-            path = "/bin:/usr/bin:/usr/local/bin";
-        }
-        astconf_path = path;
-    }
-
+    char *path = getenv("PATH");
+    if (!path || !*path) path = strdup(cs_path());
     return path;
 }

src/lib/libast/tests/path/pathbin.c
@@ -14,7 +14,7 @@ tmain() {
     UNUSED(argv);
     int failed = 0;
     char *result = NULL;
-    char default_astconf_path[BUFSIZ];
+    char default_cs_path[BUFSIZ];
 
     char *custom_path = "/foo:/bar:/baz";
 
@@ -38,14 +38,14 @@ tmain() {
     }
 
     // When `PATH` is not set, `pathbin()` gets default value from `confstr()` function
-    if (confstr(_CS_PATH, default_astconf_path, BUFSIZ) == 0 && errno == EINVAL) {
-        fprintf(stderr, "Failed to get default astconf path");
+    if (confstr(_CS_PATH, default_cs_path, BUFSIZ) == 0 && errno == EINVAL) {
+        fprintf(stderr, "Failed to get default cs_path");
         failed = 1;
     }
 
     // Check return value of `pathbin()` when `PATH` is not set
     result = pathbin();
-    if (strcmp(result, default_astconf_path)) {
+    if (strcmp(result, default_cs_path)) {
         fprintf(stderr, "pathbin() function fails when PATH is not set");
         failed = 1;
     }

src/cmd/ksh93/sh/path.c
@@ -436,9 +436,9 @@ Pathcomp_t *path_nextcomp(Shell_t *shp, Pathcomp_t *pp, const char *name, Pathco
 
 static_fn Pathcomp_t *defpath_init(Shell_t *shp) {
     if (!std_path) {
-        std_path = astconf("PATH", NULL, NULL);
+        std_path = cs_path();
         if (std_path) {
-            // Value returned by astconf() is short lived, duplicate the string.
+            // Value returned by cs_path() is short lived, duplicate the string.
             // https://github.com/att/ast/issues/959
             std_path = strdup(std_path);
         } else {

src/lib/libast/misc/procopen.c
@@ -546,7 +546,7 @@ Proc_t *procopen(const char *cmd, char **argv, char **envv, long *modv, int flag
             env[2] = 0;
             if (!sh_setenviron(env)) goto cleanup;
         }
-        if ((flags & PROC_PARANOID) && setenv("PATH", astconf("PATH", NULL, NULL), 1)) goto cleanup;
+        if ((flags & PROC_PARANOID) && setenv("PATH", cs_path(), 1)) goto cleanup;
         if ((p = envv) && p != (char **)environ) {
             while (*p) {
                 if (!sh_setenviron(*p++)) goto cleanup;

commit f68b4029d6cd6faa1d6463eaa5198f240dfd0d54

Change cs_path() implementation
Coverity Scan warns about memory leaks for code paths involving the
cs_path() function I introduced a week ago in commit 9755dce. That's a
false positive since the function uses the singleton pattern. Still, it's
easier to just change the definition to use a static fixed size buffer
rather than dynamically allocate the buffer the first time it is needed.
The alternative is to play whack-a-mole supressing these warnings every
time a new use of cs_path() is introduced.

src/lib/libast/include/ast.h
@@ -206,7 +206,7 @@ extern char *fmtre(const char *);
 extern char *fmtscale(Sfulong_t, int);
 extern void *memdup(const void *, size_t);
 extern char *pathaccess(const char *, const char *, const char *, int, char *, size_t);
-extern const char *cs_path(void);
+extern char *cs_path(void);
 extern char *pathbin(void);
 extern char *pathcanon(char *, size_t, int);
 extern char *pathcat(const char *, int, const char *, const char *, char *, size_t);

src/lib/libast/path/pathbin.c
@@ -25,26 +25,30 @@
 //
 #include "config_ast.h"  // IWYU pragma: keep
 
+#include <stdbool.h>
 #include <string.h>
 #include <unistd.h>
 
 #include "ast.h"
 
-static const char *def_cs_path = "/bin:/usr/bin:/usr/local/bin";
-static char *saved_cs_path = NULL;
 #define CS_PATH_LEN 1024
+
+static const char *def_cs_path = "/bin:/usr/bin:/usr/local/bin";
 
 //
 //  Return default PATH to find all system utilities.
 //
-const char *cs_path() {
-    if (!saved_cs_path) {
-        saved_cs_path = malloc(CS_PATH_LEN);
-        size_t rv = confstr(_CS_PATH, saved_cs_path, CS_PATH_LEN);
-        if (rv == 0) strlcpy(saved_cs_path, def_cs_path, CS_PATH_LEN);
-        // Make sure the string is null terminated in case the buf was too small.
-        saved_cs_path[CS_PATH_LEN - 1] = '\0';
-    }
+char *cs_path() {
+    static bool path_ok = false;
+    static char saved_cs_path[CS_PATH_LEN] = {0};
+
+    if (path_ok) return saved_cs_path;
+    path_ok = true;
+
+    size_t rv = confstr(_CS_PATH, saved_cs_path, CS_PATH_LEN);
+    if (rv == 0) strlcpy(saved_cs_path, def_cs_path, CS_PATH_LEN);
+    // Make sure the string is null terminated in case the buf was too small for confstr().
+    saved_cs_path[CS_PATH_LEN - 1] = '\0';
     return saved_cs_path;
 }
 
@@ -53,6 +57,6 @@ const char *cs_path() {
 //
 char *pathbin(void) {
     char *path = getenv("PATH");
-    if (!path || !*path) path = strdup(cs_path());
+    if (!path || !*path) path = cs_path();
     return path;
 }