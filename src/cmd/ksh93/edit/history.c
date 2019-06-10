212ad42ae5075197413c68d6500c74829324b8ae
kshbugbot <kshbugbot@gmail.com>
2019-06-11 00:15:43 +0200

    [kshbugbot] Fix code styling

--- src/cmd/ksh93/edit/history.c
+++ src/cmd/ksh93/edit/history.c
@@ -28,8 +28,8 @@
 #include <ctype.h>
 #include <errno.h>
 #include <fcntl.h>
-#include <stdio.h>
 #include <stdbool.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/stat.h>
