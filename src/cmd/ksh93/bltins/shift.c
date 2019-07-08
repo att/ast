8032ff7224a08cdf90bd46970fa67a3ede15814c
Kurtis Rader <krader@skepticism.us>
2019-07-07 21:12:06 -0700

    Move `shift` out of misc.c

---
+++ src/cmd/ksh93/bltins/shift.c
@@ -0,0 +1,79 @@
+/***********************************************************************
+ *                                                                      *
+ *               This software is part of the ast package               *
+ *          Copyright (c) 1982-2012 AT&T Intellectual Property          *
+ *                      and is licensed under the                       *
+ *                 Eclipse Public License, Version 1.0                  *
+ *                    by AT&T Intellectual Property                     *
+ *                                                                      *
+ *                A copy of the License is available at                 *
+ *          http://www.eclipse.org/org/documents/epl-v10.html           *
+ *         (with md5 checksum b35adb5213ca9657e911e9befb180842)         *
+ *                                                                      *
+ *              Information and Software Systems Research               *
+ *                            AT&T Research                             *
+ *                           Florham Park NJ                            *
+ *                                                                      *
+ *                    David Korn <dgkorn@gmail.com>                     *
+ *                                                                      *
+ ***********************************************************************/
+#include "config_ast.h"  // IWYU pragma: keep
+
+#include <setjmp.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+
+#include "argnod.h"
+#include "ast.h"
+#include "builtins.h"
+#include "defs.h"
+#include "error.h"
+#include "fault.h"
+#include "io.h"
+#include "jobs.h"
+#include "name.h"
+#include "option.h"
+#include "path.h"
+#include "sfio.h"
+#include "shcmd.h"
+#include "shnodes.h"
+#include "stk.h"
+#include "variables.h"
+
+//
+// Builtin `shift`.
+//
+int b_shift(int n, char *argv[], Shbltin_t *context) {
+    char *arg;
+    Shell_t *shp = context->shp;
+    while ((n = optget(argv, sh_optshift))) {
+        switch (n) {
+            case ':': {
+                errormsg(SH_DICT, 2, "%s", opt_info.arg);
+                break;
+            }
+            case '?': {
+                errormsg(SH_DICT, ERROR_usage(0), "%s", opt_info.arg);
+                return 2;
+            }
+            default: { break; }
+        }
+    }
+
+    if (error_info.errors) {
+        errormsg(SH_DICT, ERROR_usage(2), "%s", optusage(NULL));
+        __builtin_unreachable();
+    }
+
+    argv += opt_info.index;
+    n = ((arg = *argv) ? (int)sh_arith(shp, arg) : 1);
+    if (n < 0 || shp->st.dolc < n) {
+        errormsg(SH_DICT, ERROR_exit(1), e_number, arg);
+        __builtin_unreachable();
+    } else {
+        shp->st.dolv += n;
+        shp->st.dolc -= n;
+    }
+    return 0;
+}
