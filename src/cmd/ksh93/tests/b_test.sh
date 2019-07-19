c89abf515a0f5a20a19c18a75229140f95b64db8
Kurtis Rader <krader@skepticism.us>
2019-07-18 20:27:29 -0700

    Add some `test` tests to improve code coverage

---
+++ src/cmd/ksh93/tests/b_test.sh
@@ -0,0 +1,83 @@
+# TODO: Move other basic tests of the `test` and `[...]` commands from other unit test modules into
+# this unit test.
+
+# =======
+# Test that `[...]` with the closing `]` not present is an error.
+actual=$( [ x = y 2>&1 )
+status=$?
+expect=": [: ']' missing"
+[[ $actual =~ .*"$expect" ]] || log_error "missing close ] wrong error" "$expect" "$actual"
+[[ $status == 2 ]] || log_error "missing close ] wrong status" "2" "$status"
+
+# =======
+# An invalid operator is treated as an error.
+actual=$(test -@ arglebargle 2>&1)
+expect=": test: -@: unknown operator"
+[[ $actual =~ .*"$expect" ]] || log_error "unknown operator" "$expect" "$actual"
+
+# =======
+# Missing arg is an error.
+actual=$(test x = 2>&1)
+expect=": test: argument expected"
+[[ $actual =~ .*"$expect" ]] || log_error "missing arg" "$expect" "$actual"
+
+# =======
+# test -f
+# Verify file detection works.
+test -f ""
+actual=$?
+expect=1
+[[ $actual == $expect ]] || log_error "test -f" "$expect" "$actual"
+
+test -f b_test.sh  # "b_test.sh" should be a regular file
+actual=$?
+expect=0
+[[ $actual == $expect ]] || log_error "test -f" "$expect" "$actual"
+
+test -f home  # "home" should be a directory
+actual=$?
+expect=1
+[[ $actual == $expect ]] || log_error "test -f" "$expect" "$actual"
+
+# =======
+# test -b
+# Verify block device detection works.
+test -b /dev/stdout
+actual=$?
+expect=1
+[[ $actual == $expect ]] || log_error "test -b" "$expect" "$actual"
+
+if test -e /dev/disk0
+then
+    test -b /dev/disk0
+    actual=$?
+    expect=0
+    [[ $actual == $expect ]] || log_error "test -b" "$expect" "$actual"
+elif test -e /dev/sda
+then
+    test -b /dev/sda
+    actual=$?
+    expect=0
+    [[ $actual == $expect ]] || log_error "test -b" "$expect" "$actual"
+fi
+
+# =======
+# test -S
+# Verify unix domain socket detection works. This requires `nc` (netcat) to create the special file.
+if whence -p nc >/dev/null
+then
+    nc -U socket -l &
+    sleep 0.1  # need to give `nc` time to create the socket
+    test -S /dev/stdout
+    actual=$?
+    expect=1
+    [[ $actual == $expect ]] || log_error "test -S" "$expect" "$actual"
+
+    test -S socket
+    actual=$?
+    expect=0
+    [[ $actual == $expect ]] || log_error "test -S" "$expect" "$actual"
+
+    kill %1
+    wait
+fi
