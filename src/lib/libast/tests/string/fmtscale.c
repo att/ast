7a0d4ef20518855cdfad84051b8b840ef12b9479
Kurtis Rader <krader@skepticism.us>
2019-07-24 23:45:35 -0700

    Add fmtscale() tests
    The fmtscale() function has poor test coverage. This improves it.
    See also https://github.com/att/ast/issues/1366 where I question whether
    the current behavior is sensible.

--- src/lib/libast/tests/string/fmtscale.c
+++ src/lib/libast/tests/string/fmtscale.c
@@ -6,40 +6,59 @@
 #include "terror.h"
 
 tmain() {
+    texit(0);
     UNUSED(argc);
     UNUSED(argv);
 
     const char *actual_result = NULL;
 
-    struct IntegerToStringTest test1[] = {{1000, "1.0k"},
-                                          {2095, "2.1k"},
-                                          {9 * 1024 * 1024, "9.4M"},
-                                          {1024 * 1024 * 1024, "1.1G"},
-                                          {0, NULL}};
+    // Test integer conversions using scale == 1000.
+    struct IntegerToStringTest test1000[] = {{0, "0"},  // the most basic test
+                                             {1, "1.0"},
+                                             {5, "5.0"},
+                                             {9, "9.0"},
+                                             {10, "10"},
+                                             {666, "666"},
+                                             {999, "999"},
+                                             {1000, "1.0k"},
+                                             {1024, "1.0k"},
+                                             {1025, "1.0k"},
+                                             {1524, "1.5k"},
+                                             {2095, "2.1k"},
+                                             {9 * 1024 * 1024, "9.4M"},
+                                             {1024 * 1024 * 1024, "1.1G"},
+                                             {0, NULL}};
 
-    struct IntegerToStringTest test2[] = {{1024, "1.0Ki"},
-                                          {2095, "2.0Ki"},
-                                          {9 * 1024 * 1024, "9.0Mi"},
-                                          {1024 * 1024 * 1024, "1.0Gi"},
-                                          {0, NULL}};
+    // Test integer conversions using scale == 1024.
+    struct IntegerToStringTest test1024[] = {{0, "0"},  // the most basic test
+                                             {1, "1.0"},
+                                             {7, "7.0"},
+                                             {9, "9.0"},
+                                             {10, "10"},
+                                             {666, "666"},
+                                             {999, "999"},
+                                             {1000, "1.0Ki"},
+                                             {1024, "1.0Ki"},
+                                             {1025, "1.0Ki"},
+                                             {1524, "1.5Ki"},
+                                             {2095, "2.0Ki"},
+                                             {9 * 1024 * 1024, "9.0Mi"},
+                                             {1024 * 1024 * 1024, "1.0Gi"},
+                                             {0, NULL}};
 
-    for (int i = 0; test1[i].input; ++i) {
-        actual_result = fmtscale(test1[i].input, 1000);
-        if (strcmp(actual_result, test1[i].expected_result)) {
-            terror(
-                "fmtscale() failed :: Failed to convert number to string :: Actual Result : %s, "
-                "Expected Result : %s",
-                actual_result, test1[i].expected_result);
+    for (int i = 0; test1000[i].expected_result; ++i) {
+        actual_result = fmtscale(test1000[i].input, 1000);
+        if (strcmp(actual_result, test1000[i].expected_result)) {
+            terror("fmtscale() %d: %d:\nActual: %s\nExpect: %s", i, test1000[i].input,
+                   actual_result, test1000[i].expected_result);
         }
     }
 
-    for (int i = 0; test2[i].input; ++i) {
-        actual_result = fmtscale(test2[i].input, 1024);
-        if (strcmp(actual_result, test2[i].expected_result)) {
-            terror(
-                "fmtscale() failed :: Failed to convert number to string :: Actual Result : %s, "
-                "Expected Result : %s",
-                actual_result, test2[i].expected_result);
+    for (int i = 0; test1024[i].expected_result; ++i) {
+        actual_result = fmtscale(test1024[i].input, 1024);
+        if (strcmp(actual_result, test1024[i].expected_result)) {
+            terror("fmtscale() %d: %d:\nActual: %s\nExpect: %s", i, test1024[i].input,
+                   actual_result, test1024[i].expected_result);
         }
     }
 
