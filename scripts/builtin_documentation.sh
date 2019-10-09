att/ast

commit 44fb5d5aa8c0a00e048ac475055c1c137497adcf

Move the other builtin command docs into ext files
This builds on the prior change to extricate builtin command documentation
out of the source and into an external text file for each command.
Which is the next step into converting the documentation to a modern
markup language.

Related #507

src/cmd/ksh93/docs/ksh_mini.1
@@ -1,0 +1,37 @@
++[-1?
+@(#)$Id: sh (AT&T Research) $
+]
+[+NAME?\b\f?\f\b - Shell, the standard command language interpreter]
+[+DESCRIPTION?\b\f?\f\b is a command language interpreter that executes commands read from a command line string, the standard input, or a specified file.]
+[+?If the \b-i\b option is present, or there are no \aarg\as and the standard input and standard error are attached to a terminal, the shell is considered to be interactive.]
+[+?The \b-s\b and \b-c\b options are mutually exclusive.  If the \b-c\b option is specified, the first \aarg\a is the command-line string and must be specified.  Any remaining \aarg\as will be used to initialize \b$0\b and positional parameters.]
+[+?If the neither \b-s\b nor \b-c\b is specified, then the first \barg\b will be the pathname of the file containing commands and \b$0\b will be set to this value.  If there is no file with this pathname, and this pathame does not contain a \b/\b, then the \bPATH\b will be searched for an executable with this name.  Any remaining \aarg\as will be used to initialize the positional parmaeters.]
+[+?Any option can use a \b+\b instead of a \b-\b to disable the corresponding option.]
+[c?Read the commands from the first \aarg\a.]
+[i?Specifies that the shell is interactive.]
+[l?Invoke the shell as a login shell; \b/etc/profile\b and \b$HOME/.profile\b, if they exist, are read before the first command.]
+[r\f:restricted\f?Invoke the shell in a restricted mode.  A restricted shell does not permit any of the following:]{
+[+-?Changing the working directory.]
+[+-?Setting values or attributes of the variables \bSHELL\b, \bENV\b, \bFPATH\b, or \bPATH\b.]
+[+-?Executing any command whose name as a \b/\b in it.]
+[+-?Redirecting output of a command with \b>\b, \b>|\b, \b<>\b, or \b>>\b.]
+[+-?Adding or deleting built-in commands or libraries with \bbuiltin\b.]
+[+-?Executing \bcommand -p\b \a...\a .]
+}
+[s?Read the commands from standard input.  The positional parameters will be initialized from \aarg\a.]
+[D\f:dump-strings\f?Do not execute the script, but output the set of double quoted strings preceded by a \b$\b.  These strings are needed for localization of the script to different locales.]
+[E?Reads the file \b/etc/ksh.kshrc\b, if it exists, as a profile, followed by \b${ENV-$HOME/.kshrc}\b, if it exists, as a profile. On by default for interactive shells; use \b+E\b to disable.]
+[R]:[file?Do not execute the script, but create a cross reference database in \afile\a that can be used a separate shell script browser.  The -R option requires a script to be specified as the first operand.]
+\fabc\f
+?
+[T?Enable implementation specific test code defined by mask.]#[mask]
+
+[arg ...]
+
+[+EXIT STATUS?If \b\f?\f\b executes command, the exit status will be that of the last command executed.  Otherwise, it will be one of the following:]{
+[+0?The script or command line to be executed consists entirely of zero or more blank lines or comments.]
+[+>1-125?A noninteractive shell detected a syntax error, a variable assignment error, or an error in a special built-in.]
+[+126?\b-c\b and \b-s\b were not specified and the command script was found on \bPATH\b but was not executable.]
+[+127?\b-c\b and \b-s\b were not specified and the command script corresponding to \aarg\a could not be found.]
+}
+[+SEE ALSO?\bset\b(1), \bbuiltin\b(1)]

src/cmd/ksh93/docs/alias.1
@@ -1,0 +1,18 @@
+[-1c?
+@(#)$Id: alias (AT&T Research) 1999-07-07 $
+]
+[+NAME?alias - define or display aliases]
+[+DESCRIPTION?\balias\b creates or redefines alias definitions or writes the existing alias definitions to standard output.  An alias definitions provides a string value that will replace a command name when the command is read.  Alias names can contain any printable character which is not special to the shell.  If an alias value ends in a space or tab, then the word following the command name the alias replaces is also checked to see whether it is an alias.]
+[+?If no \aname\as are specified then the names and values of all aliases are written to standard output.  Otherwise, for each \aname\a that is specified, and \b=\b\avalue\a  is not specified, the current value of the alias corresponding to \aname\a is written to standard output.  If \b=\b\avalue\a is specified, the alias \aname\a will be created or redefined.]
+[+?\balias\b is built-in to the shell as a declaration command so that field splitting and pathname expansion are not performed on the arguments.  Tilde expansion occurs on \avalue\a.  An alias definition only affects scripts read by the current shell environment.  It does not effect scripts run by this shell.]
+[p?Causes the output to be in the form of alias commands that can be used as input to the shell to recreate the current aliases.]
+[t?Used for tracked aliases.  These are aliases that connect a command name to the pathname of the command and are reset when the \bPATH\b variable is unset.  The tracked aliases feature is now obsolete.]
+[x?Ignored, this option is obsolete.]
+
+[name[=value]...]
+
+[+EXIT STATUS?]{
+[+0?Successful completion.]
+[+>0?One or more \aname\a operands did not have an alias definition, or an error occurred.]
+}
+[+SEE ALSO?\bsh\b(1), \bunalias\b(1)]

src/cmd/ksh93/docs/bg.1
@@ -1,0 +1,23 @@
+[-1c?
+@(#)$Id: bg (AT&T Research) 2000-04-02 $
+]
+[+NAME?bg - resume jobs in the background]
+[+DESCRIPTION?\bbg\b places the given \ajob\as into the background and sends them a \bCONT\b signal to start them running.]
+[+?If \ajob\a is omitted, the most recently started or stopped background job is resumed or continued in the background.]
+[+?Each \ajob\a can be specified as one of the following:]{
+[+\anumber\a?\anumber\a refers to a process id.]
+[+-\anumber\a?\anumber\a refers to a process group id.]
+[+%\anumber\a?\anumber\a refer to a job number.]
+[+%\astring\a?Refers to a job whose name begins with \astring\a.]
+[+%??\astring\a?Refers to a job whose name contains \astring\a.]
+[+%+ \bor\b %%?Refers to the current job.]
+[+%-?Refers to the previous job.]
+}
+
+[job ...]
+
+[+EXIT STATUS?]{
+[+0?If all background jobs are started.]
+[+>0?If one more jobs does not exist or there are no background jobs.]
+}
+[+SEE ALSO?\bwait\b(1), \bfg\b(1), \bdisown\b(1), \bjobs\b(1)]

src/cmd/ksh93/docs/break.1
@@ -1,0 +1,11 @@
+[-1c?
+@(#)$Id: break (AT&T Research) 1999-04-07 $
+]
+[+NAME?break - break out of loop ]
+[+DESCRIPTION?\bbreak\b is a shell special built-in that exits the smallest enclosing \bfor\b, \bselect\b, \bwhile\b, or \buntil\b loop, or the \an\a-th enclosing loop if \an\a is specified.  Execution continues at the command following the loop(s).]
+[+?If \an\a is given, it must be a positive integer >= 1. If \an\a is larger than the number of enclosing loops, the last enclosing loop will be exited.]
+
+[n]
+
+[+EXIT STATUS?0]
+[+SEE ALSO?\bcontinue\b(1), \breturn\b(1)]

src/cmd/ksh93/docs/builtin.1
@@ -1,0 +1,24 @@
+[-1c?
+@(#)$Id: builtin (AT&T Research) 2014-06-05 $
+]
+[+NAME?builtin - add, delete, or display shell built-ins]
+[+DESCRIPTION?\bbuiltin\b can be used to add, delete, or display built-in commands in the current shell environment. A built-in command executes in the current shell process and can have side effects in the current shell. On most systems, the invocation time for built-in commands is one or two orders of magnitude less than commands that create a separate process.]
+[+?For each \apathname\a specified, the basename of the pathname determines the name of the built-in. For each basename, the shell looks for a C level function in the current shell whose name is determined by prepending \bb_\b to the built-in name. If \apathname\a contains a \b/\b, then the built-in is bound to this pathname. A built-in bound to a pathname will only be executed if \apathname\a is the first executable found during a path search. Otherwise, built-ins are found prior to performing the path search.]
+[+?If no \apathname\a operands are specified, then \bbuiltin\b displays the current list of built-ins, or just the special built-ins if \b-s\b is specified, on standard output. The full pathname for built-ins that are bound to pathnames are displayed.]
+[+?Libraries containing built-ins can be specified with the \b-f\b option. If the library contains a function named \blib_init\b(), this function will be invoked with argument \b0\b when the library is loaded. The \blib_init\b() function can load built-ins by invoking an appropriate C level function. In this case there is no restriction on the C level function name.]
+[+?The C level function will be invoked with three arguments. The first two are the same as \bmain\b() and the third one is a pointer.]
+[+?\bbuiltin\b cannot be invoked from a restricted shell.]
+[d?Deletes each of the specified built-ins. Special built-ins cannot be deleted.]
+[f]:[lib?On systems with dynamic linking, \alib\a names a shared library to load and search for built-ins. Libraries are searched for in \b../lib/ksh\b and \b../lib\b on \b$PATH\b and in system dependent library directories. The system dependent shared library prefix and/or suffix may be omitted. Once a library is loaded, its symbols become available for the current and subsequent invocations of \bbuiltin\b. Multiple libraries can be specified with separate invocations of \bbuiltin\b. Libraries are searched in the reverse order in which they are specified.]
+[n?Disable each of the specified built-ins. Special built-ins cannot be disabled.  If no built-ns are specifed, display all disabled built-ins.]
+[l?List the library base name, plugin YYYYMMDD version stamp, and full path for \b-f\b\alib\a on one line on the standard output.]
+[p?Causes the output to be in a form of \bbuiltin\b commands that can be used as input to the shell to recreate the current set of builtins.]
+[s?Display only the special built-ins.]
+
+[pathname ...]
+
+[+EXIT STATUS?]{
+[+0?All \apathname\a operands and \b-f\b options processed successfully.]
+[+>0?An error occurred.]
+}
+[+SEE ALSO?\bwhence\b(1)]

src/cmd/ksh93/docs/cd.1
@@ -1,0 +1,23 @@
+[-1c?
+@(#)$Id: cd (AT&T Research) 2013-08-18 $
+]
+[+NAME?cd - change working directory ]
+[+DESCRIPTION?\bcd\b changes the current working directory of the current shell environment.]
+[+?In the first form with one operand, if \adirectory\a begins with \b/\b, or if the first component is \b.\b or \b..\b, the directory will be changed to this directory.  If directory is \b-\b, the directory will be changed to the last directory visited.  Otherwise, if the \bCDPATH\b environment variable is set, \bcd\b searches for \adirectory\a relative to each directory named in the colon separated list of directories defined by \bCDPATH\b.  If \bCDPATH\b not set, \bcd\b changes to the directory specified by \adirectory\a.]
+[+?In the second form, the first occurrence of the string \aold\a contained in the pathname of the present working directory is replaced by the string \anew\a and the resulting string is used as the directory to which to change.]
+[+?When invoked without operands and when the \bHOME\b environment variable is set to a nonempty value,  the directory named by the \bHOME\b environment variable will be used.  If \bHOME\b is empty or unset, it will use the home directory for the effective user if possible.  Otherwise \bcd\b will fail.]
+[+?When \bcd\b is successful, the \bPWD\b environment variable will be set to the name of an absolute pathname that does not contain any \b..\b components corresponding to the new directory.  The environment variable \bOLDPWD\b will be set to the previous value of \bPWD\b.  If the new directory is found by searching the directories named by \bCDPATH\b, or if \adirectory\a is \b-\b, or if the two operand form is used, the new value of \bPWD\b will be written to standard output.]
+[+?If both \b-L\b and \b-P\b are specified, the last one specified will be used.  If neither \b-P\b or \b-L\b is specified the default behavior is as if \b-L\b was specified.]
+[f?The directory path is relative to the open file descriptor \adirfd\a.]#[dirfd]
+[L?Handle each pathname component \b..\b in a logical fashion by moving up one level by name in the present working directory.]
+[P?The present working directory is first converted to an absolute pathname that does not contain symbolic link components and symbolic name components are expanded in the resulting directory name.]
+[@?Change into the hidden attribute directory of \adirectory\a which may also be a file. \bCDPATH\b is ignored. Hidden attribute directories are file system and operating system specific.]
+
+[directory]
+old new
+
+[+EXIT STATUS?]{
+[+0?Directory successfully changed.]
+[+>0?An error occurred.]
+}
+[+SEE ALSO?\bpwd\b(1)]

src/cmd/ksh93/docs/command.1
@@ -1,0 +1,20 @@
+[-1c?
+@(#)$Id: command (AT&T Research) 2003-08-01 $
+]
+[+NAME?command - execute a simple command]
+[+DESCRIPTION?Without \b-v\b or \b-V\b,  \bcommand\b executes \acommand\a with arguments given by \aarg\a, suppressing the shell function lookup that normally occurs.  In addition, if \acommand\a is a special built-in command, then the special properties are removed so that failures will not cause the script that executes it to terminate.]
+[+?With the \b-v\b or \b-V\b options, \bcommand\b is equivalent to the \bwhence\b(1) command.]
+[p?Causes a default path to be searched rather than the one defined by the value of \bPATH\b.]
+[v?Equivalent to \bwhence\b \acommand\a [\aarg\a ...]].]
+[x?If \acommand\a fails because there are too many \aarg\as, it will be invoked multiple times with a subset of the arguments on each invocation.  Arguments that occur prior to the first word that expand to multiple arguments and arguments that occur after the last word that expands to multiple arguments will be passed on each invocation. The exit status will be the maximum invocation exit status.]
+[V?Equivalent to \bwhence \b-v\b \acommand\a [\aarg\a ...]].]
+
+[command [arg ...]]
+
+[+EXIT STATUS?If \acommand\a is invoked, the exit status of \bcommand\b will be that of \acommand\a.  Otherwise, it will be one of the following:]{
+[+0?\bcommand\b completed successfully.]
+[+>0?\b-v\b or \b-V\b has been specified and an error occurred.]
+[+126?\acommand\a was found but could not be invoked.]
+[+127?\acommand\a could not be found.]
+}
+[+SEE ALSO?\bwhence\b(1), \bgetconf\b(1)]

commit 07811593bff2569265a331a34401dc19bf31b05e

Show version number through `ksh --version`
Version number is stored in `SH_RELEASE` macro. Add it to the output of
`ksh --version`. This was a regression introduced by 44fb5d5a.

scripts/builtin_documentation.sh
@@ -3,6 +3,8 @@
 # the raw markup text.
 out_file="$MESON_BUILD_ROOT/$MESON_SUBDIR/documentation.c"
 exec > "$out_file"
+# `ksh_mini.1` uses `SH_VERSION` macro to generate version number string
+echo '#include "version.h"'
 for in_file in "$MESON_SOURCE_ROOT/src/cmd/ksh93/docs/"*.1
 do
     cmd_name=$(basename "$in_file" .1)

src/cmd/ksh93/docs/ksh_mini.1
@@ -1,5 +1,5 @@
 +[-1?
-@(#)$Id: sh (AT&T Research) $
+@(#)$Id: sh (AT&T Research) " SH_RELEASE " $
 ]
 [+NAME?\b\f?\f\b - Shell, the standard command language interpreter]
 [+DESCRIPTION?\b\f?\f\b is a command language interpreter that executes commands read from a command line string, the standard input, or a specified file.]