att/ast

commit 6542e5a39dcfc5badc8999e3e02bff2243174860

Coverity CID#287603 Resource leak
This fixes a potential leak of a file descriptor. In practice this can't
happen except for pathological situations. Specifically, the open()
succeeds but the subsequent fstat() fails because of an OS or network
file system glitch.

src/cmd/ksh93/sh/path.c
@@ -58,7 +58,7 @@
 #define RW_ALL (S_IRUSR | S_IRGRP | S_IROTH | S_IWUSR | S_IWGRP | S_IWOTH)
 #define LIBCMD "cmd"
 
-static_fn int canexecute(Shell_t *, char *, int);
+static_fn int can_execute(Shell_t *, char *, bool);
 static_fn void funload(Shell_t *, int, const char *);
 static_fn void exscript(Shell_t *, char *, char *[], char *const *);
 static_fn bool path_chkpaths(Shell_t *, Pathcomp_t *, Pathcomp_t *, Pathcomp_t *, int);
@@ -579,7 +579,7 @@ bool path_search(Shell_t *shp, const char *name, Pathcomp_t **oldpp, int flag) {
     if (strchr(name, '/')) {
         stkseek(shp->stk, PATH_OFFSET);
         sfputr(shp->stk, name, -1);
-        if (canexecute(shp, stkptr(shp->stk, PATH_OFFSET), 0) < 0) {
+        if (can_execute(shp, stkptr(shp->stk, PATH_OFFSET), false) < 0) {
             *stkptr(shp->stk, PATH_OFFSET) = 0;
             return false;
         }
@@ -671,7 +671,7 @@ Pathcomp_t *path_absolute(Shell_t *shp, const char *name, Pathcomp_t *pp) {
             return NULL;
         }
         isfun = (oldpp->flags & PATH_FPATH);
-        if (!isfun && *oldpp->name == '.' && oldpp->name[1] == 0 && pwdinfpath()) isfun = 1;
+        if (!isfun && *oldpp->name == '.' && oldpp->name[1] == 0 && pwdinfpath()) isfun = true;
         if (!isfun && !sh_isoption(shp, SH_RESTRICTED)) {
             char *bp;
             Shbltin_f addr;
@@ -764,7 +764,7 @@ Pathcomp_t *path_absolute(Shell_t *shp, const char *name, Pathcomp_t *pp) {
         }
         shp->bltin_dir = 0;
         sh_stats(STAT_PATHS);
-        f = canexecute(shp, stkptr(shp->stk, PATH_OFFSET), isfun);
+        f = can_execute(shp, stkptr(shp->stk, PATH_OFFSET), isfun);
         if (isfun && f >= 0 && (cp = strrchr(name, '.'))) {
             *cp = 0;
             if (nv_open(name, sh_subfuntree(shp, 1), NV_NOARRAY | NV_IDENT | NV_NOSCOPE)) f = -1;
@@ -813,13 +813,14 @@ Pathcomp_t *path_absolute(Shell_t *shp, const char *name, Pathcomp_t *pp) {
 #endif  // S_IEXEC
 #endif  // S_IXUSR
 
-static_fn int canexecute(Shell_t *shp, char *path, int isfun) {
+static_fn int can_execute(Shell_t *shp, char *path, bool isfun) {
     struct stat statb;
     int fd = 0;
 
     path = path_relative(shp, path);
     if (isfun) {
-        if ((fd = open(path, O_RDONLY | O_CLOEXEC, 0)) < 0 || fstat(fd, &statb) < 0) goto err;
+        fd = sh_open(path, O_RDONLY | O_CLOEXEC, 0);
+        if (fd < 0 || fstat(fd, &statb) < 0) goto err;
     } else if (stat(path, &statb) < 0) {
 #if __CYGWIN__
         // Check for .exe or .bat suffix.
@@ -841,11 +842,12 @@ static_fn int canexecute(Shell_t *shp, char *path, int isfun) {
     errno = EPERM;
     if (S_ISDIR(statb.st_mode)) {
         errno = EISDIR;
-    } else if (isfun == 1 || (statb.st_mode & S_IXALL) == S_IXALL || sh_access(path, X_OK) >= 0) {
+    } else if (isfun || (statb.st_mode & S_IXALL) == S_IXALL || sh_access(path, X_OK) >= 0) {
         return fd;
     }
-    if (isfun && fd >= 0) sh_close(fd);
+
 err:
+    if (isfun && fd >= 0) sh_close(fd);
     return -1;
 }
 

commit 6928edc8bab3155eb006cacd9175ebaf44ca3210

Fix bug introduced by commit 6542e5a3
It turns out that despite the name, `isfun`, and how the code seems to
manipulate it the var is really tri-state: 0, 1, 2 (PATH_FPATH). This
also correctly fixes the file descriptor leak that should be impossible
but given how can_execute() was structured could occur.

src/cmd/ksh93/include/path.h
@@ -28,14 +28,14 @@
 #include "name.h"
 #include "shell.h"
 
-#define PATH_PATH 0001
-#define PATH_FPATH 0002
-#define PATH_CDPATH 0004
-#define PATH_BFPATH 0010
-#define PATH_SKIP 0020
-#define PATH_BUILTIN_LIB 0040
-#define PATH_STD_DIR 0100  // directory is on  $(getconf PATH)
-#define PATH_BIN 0200      // path behaves like /bin for builtins
+#define PATH_PATH (1 << 0)
+#define PATH_FPATH (1 << 1)  // do not change this value; see can_execute() and path_absolute()
+#define PATH_CDPATH (1 << 2)
+#define PATH_BFPATH (1 << 3)
+#define PATH_SKIP (1 << 4)
+#define PATH_BUILTIN_LIB (1 << 5)
+#define PATH_STD_DIR (1 << 6)  // directory is on  $(getconf PATH)
+#define PATH_BIN (1 << 7)      // path behaves like /bin for builtins
 
 #define PATH_OFFSET 2  // path offset for path_join
 #define MAXDEPTH 1024  // maximum recursion depth

src/cmd/ksh93/sh/path.c
@@ -65,7 +65,7 @@
 #define O_DIRECTORY 0
 #endif
 
-static_fn int can_execute(Shell_t *, char *, bool);
+static_fn int can_execute(Shell_t *, char *, int);
 static_fn void funload(Shell_t *, int, const char *);
 static_fn void exscript(Shell_t *, char *, char *[], char *const *);
 static_fn bool path_chkpaths(Shell_t *, Pathcomp_t *, Pathcomp_t *, Pathcomp_t *, int);
@@ -618,7 +618,7 @@ bool path_search(Shell_t *shp, const char *name, Pathcomp_t **oldpp, int flag) {
     if (strchr(name, '/')) {
         stkseek(shp->stk, PATH_OFFSET);
         sfputr(shp->stk, name, -1);
-        if (can_execute(shp, stkptr(shp->stk, PATH_OFFSET), false) < 0) {
+        if (can_execute(shp, stkptr(shp->stk, PATH_OFFSET), 0) < 0) {
             *stkptr(shp->stk, PATH_OFFSET) = 0;
             return false;
         }
@@ -689,7 +689,7 @@ static_fn bool pwdinfpath(void) {
 // Do a path search and find the full pathname of file name.
 //
 Pathcomp_t *path_absolute(Shell_t *shp, const char *name, Pathcomp_t *pp) {
-    int isfun;
+    int isfun;  // this is not a bool; it is a tri-state var: 0, 1, 2 (PATH_FPATH)
     int fd = -1;
     int noexec = 0;
     Pathcomp_t *oldpp;
@@ -713,7 +713,7 @@ Pathcomp_t *path_absolute(Shell_t *shp, const char *name, Pathcomp_t *pp) {
             return NULL;
         }
         isfun = (oldpp->flags & PATH_FPATH);
-        if (!isfun && *oldpp->name == '.' && oldpp->name[1] == 0 && pwdinfpath()) isfun = true;
+        if (!isfun && *oldpp->name == '.' && oldpp->name[1] == 0 && pwdinfpath()) isfun = 1;
         if (!isfun && !sh_isoption(shp, SH_RESTRICTED)) {
             char *bp;
             Shbltin_f addr;
@@ -867,14 +867,14 @@ Pathcomp_t *path_absolute(Shell_t *shp, const char *name, Pathcomp_t *pp) {
 #endif  // S_IEXEC
 #endif  // S_IXUSR
 
-static_fn int can_execute(Shell_t *shp, char *path, bool isfun) {
+static_fn int can_execute(Shell_t *shp, char *path, int isfun) {
     struct stat statb;
-    int fd = 0;
+    int fd = 0;  // this is deliberately set to stdin rather than -1
 
     path = path_relative(shp, path);
     if (isfun) {
         fd = sh_open(path, O_RDONLY | O_CLOEXEC, 0);
-        if (fd < 0 || fstat(fd, &statb) < 0) goto err;
+        if (fd == -1 || fstat(fd, &statb) == -1) goto err;
     } else if (sh_stat(path, &statb) < 0) {
 #if __CYGWIN__
         // Check for .exe or .bat suffix.
@@ -896,12 +896,12 @@ static_fn int can_execute(Shell_t *shp, char *path, bool isfun) {
     errno = EPERM;
     if (S_ISDIR(statb.st_mode)) {
         errno = EISDIR;
-    } else if (isfun || (statb.st_mode & S_IXALL) == S_IXALL || sh_access(path, X_OK) >= 0) {
+    } else if (isfun == 1 || (statb.st_mode & S_IXALL) == S_IXALL || sh_access(path, X_OK) >= 0) {
         return fd;
     }
+    if (isfun && fd >= 0) sh_close(fd);
 
 err:
-    if (isfun && fd >= 0) sh_close(fd);
     return -1;
 }
 